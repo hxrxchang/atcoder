5cc0523219c5b56667ade1e4b038d88008ff4adc
hxrxchang <mail@hxrxchang.dev>
2022-11-15 02:03:39 +0900

    do 024

--- typical90/024/main.py
+++ typical90/024/main.py
@@ -1,1 +1,20 @@
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+ans = "Yes"
+diff = 0
+for i in range(N):
+  a = A[i]
+  b = B[i]
+  diff += abs(a - b)
+
+if diff > K:
+  print("No")
+  exit()
+if diff % 2 != K % 2:
+  print("No")
+  exit()
+
+print("Yes")
 
