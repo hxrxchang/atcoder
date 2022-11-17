bbfd15100e00eec12886bccff78dd8e33887334b
hxrxchang <mail@hxrxchang.dev>
2022-11-17 18:54:44 +0900

    solve D

--- abc125/D/main.py
+++ abc125/D/main.py
@@ -1,1 +1,17 @@
+N = int(input())
+A = list(map(int, input().split()))
+cnt = sum([i < 0 for i in A])
+
+if cnt % 2 == 0:
+  ans = sum([abs(i) for i in A])
+  print(ans)
+else:
+  A2 = [abs(i) for i in A]
+  target = A2[0]
+  for a in A2:
+    if a < target:
+      target = a
+  A2.remove(target)
+  ans = sum(A2) - target
+  print(ans)
 
