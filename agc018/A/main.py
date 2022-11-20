5e601d547843774bad52c8d1a173321c1a7f47f8
hxrxchang <mail@hxrxchang.dev>
2022-11-21 01:03:59 +0900

    solve A

--- agc018/A/main.py
+++ agc018/A/main.py
@@ -1,1 +1,20 @@
+N, K = map(int, input().split())
+
+A = list(map(int, input().split()))
+A.sort()
+
+def GCD(x, y):
+  if y == 0:
+    return x
+  return GCD(y, x%y)
+
+max = A[-1]
+g = 0
+for i in range(N):
+  g = GCD(g, A[i])
+
+if K <= max and K % g == 0:
+  print("POSSIBLE")
+else:
+  print("IMPOSSIBLE")
 
