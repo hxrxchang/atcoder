177078fa72d1f4c9888cdb9efe1fb561b768ee70
hxrxchang <mail@hxrxchang.dev>
2022-12-02 07:18:15 +0900

    fix A

--- agc018/A/main.py
+++ agc018/A/main.py
@@ -1,20 +1,18 @@
-N, K = map(int, input().split())
+import math
 
+N, K = map(int, input().split())
 A = list(map(int, input().split()))
 A.sort()
 
-def GCD(x, y):
-  if y == 0:
-    return x
-  return GCD(y, x%y)
+if K > A[-1]:
+  print("IMPOSSIBLE")
+  exit()
 
-max = A[-1]
-g = 0
+gcd = 0
 for i in range(N):
-  g = GCD(g, A[i])
+  gcd = math.gcd(gcd, A[i])
 
-if K <= max and K % g == 0:
+if K % gcd == 0:
   print("POSSIBLE")
 else:
   print("IMPOSSIBLE")
-
