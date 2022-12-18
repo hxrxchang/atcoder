8f481ba9004eeeeeb8df34cf8b83f256583ae9d9
hxrxchang <mail@hxrxchang.dev>
2022-12-19 00:22:11 +0900

    solve C

--- abc079/C/main.py
+++ abc079/C/main.py
@@ -1,1 +1,16 @@
+S = input()
+A, B, C, D = [int(s) for s in S]
+op_cnt = len(S) - 1
+for i in range(2 ** op_cnt):
+  op = ["-"] * op_cnt
+  for j in range(op_cnt):
+    if ((i >> j) & 1):
+      op[op_cnt - 1 - j] = "+"
+
+  formula = ""
+  for a, b in zip(S, op + [""]):
+    formula += (a + b)
+  if eval(formula) == 7:
+    print(formula + "=7")
+    break
 
