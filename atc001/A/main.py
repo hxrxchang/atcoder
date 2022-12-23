d3786333f9ab2b9a24df5cc09d30f05c79b0415d
hxrxchang <mail@hxrxchang.dev>
2022-12-23 22:24:26 +0900

    solve A

--- atc001/A/main.py
+++ atc001/A/main.py
@@ -1,1 +1,40 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
 
+H, W = map(int, input().split())
+graph = []
+visited = []
+s = None
+g = None
+for i in range(H):
+  row = input()
+  if "s" in row:
+    j = row.index("s")
+    s = [i, j]
+  if "g" in row:
+    j = row.index("g")
+    g = [i, j]
+  graph.append(row)
+  visited_row = [False] * W
+  visited.append(visited_row)
+
+
+def dfs(x, y):
+  if x < 0 or y < 0 or x >= H or y >= W:
+    return
+  if graph[x][y] == "g":
+    print("Yes")
+    exit()
+  if graph[x][y] == "#":
+    return
+  if visited[x][y] == True:
+    return
+  else:
+    visited[x][y] = True
+  dfs(x - 1, y)
+  dfs(x + 1, y)
+  dfs(x, y + 1)
+  dfs(x, y - 1)
+
+dfs(s[0], s[1])
+print("No")
