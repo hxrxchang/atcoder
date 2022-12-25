987ae34d3b08b03b61c213a864e53dfd6d891f5b
hxrxchang <mail@hxrxchang.dev>
2022-12-25 18:52:39 +0900

    fix A

--- typical-algorithm/A/main.py
+++ typical-algorithm/A/main.py
@@ -1,16 +1,16 @@
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
 
-if K > A[-1]:
+ng = -1
+ok = N
+
+if A[-1] < K:
   print(-1)
 else:
-  left = 0
-  right = N - 1
-  while not left == right:
-    mid = (left + right) // 2
+  while not ok - ng == 1:
+    mid = (ok + ng) // 2
     if A[mid] >= K:
-      right = mid
+      ok = mid
     else:
-      left = mid + 1
-  print(left)
-
+      ng = mid
+  print(ok)
