fec2717a46b75232ad130c6abbaf7c587bc599c6
hxrxchang <mail@hxrxchang.dev>
2022-12-25 19:07:10 +0900

    solve C

--- abc146/C/main.py
+++ abc146/C/main.py
@@ -1,1 +1,16 @@
+A, B, X = map(int, input().split())
 
+def price(n):
+  return A * n + B * len(str(n))
+
+ok = 0
+ng = 10 ** 9 + 1
+
+while not ng - ok == 1:
+  mid = (ok + ng) // 2
+  if price(mid) <= X:
+    ok = mid
+  else:
+    ng = mid
+
+print(ok)
