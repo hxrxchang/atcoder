42d2193f9c7230f40b57a0290a00fdc87771806a
hxrxchang <mail@hxrxchang.dev>
2022-12-28 19:29:43 +0900

    理解

--- atc001/B/main.py
+++ atc001/B/main.py
@@ -1,44 +1,31 @@
-N, Q = map(int, input().split())
-
 class UnionFind:
   def __init__(self, n):
-    self.n = n
     self.parents = list(range(n))
-    self.size = [1] * n
 
-  # 要素iのルートを返す
-  def find(self, i):
-    while self.parents[i] != i:
-      self.parents[i] = self.parents[self.parents[i]]
-      i = self.parents[i]
-    return i
+  def root(self, i):
+    if self.parents[i] == i:
+      return i
+    self.parents[i] = self.root(self.parents[i])
+    return self.parents[i]
 
-  # iを含む木とjを含む木を結合する
-  def union(self, i, j):
-    if self.find(i) == self.find(j):
+  def unit(self, i, j):
+    i = self.root(i)
+    j = self.root(j)
+    if i == j:
       return
-    elif self.size[self.find(i)] < self.size[self.find(j)]:
-      self.size[self.find(j)] = self.size[self.find(i)] + self.size[self.find(j)]
-      self.parents[self.find(i)] = self.find(j)
     else:
-      self.size[self.find(i)] = self.size[self.find(j)] + self.size[self.find(i)]
-      self.parents[self.find(j)] = self.find(i)
+      self.parents[i] = j
 
   def is_same(self, i, j):
-    if self.find(i) == self.find(j):
-      return True
-    else:
-      return False
+    return self.root(i) == self.root(j)
 
-  def get_size(self, i):
-    return self.size[self.find(i)]
-
+N, Q = map(int, map(int, input().split()))
 uf = UnionFind(N)
 for _ in range(Q):
   P, A, B = map(int, input().split())
   if P == 0:
-    uf.union(A, B)
-  else:
+    uf.unit(A, B)
+  elif P == 1:
     if uf.is_same(A, B):
       print("Yes")
     else:
