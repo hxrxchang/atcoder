37343a6d30a0f7d69a34cbdf7735e5b5c0bb375e
hxrxchang <mail@hxrxchang.dev>
2023-01-01 16:38:20 +0900

    solve C

--- abc173/C/main.py
+++ abc173/C/main.py
@@ -1,1 +1,17 @@
+H, W, K = map(int, input().split())
 
+graph = [input() for _ in range(H)]
+
+ans = 0
+for h in range(2 ** H):
+  for w in range(2 ** W):
+    black_cnt = 0
+    for h2 in range(H):
+      for w2 in range(W):
+        # フラグが立っているということは、その行 or 列が赤くなるろいうことなので、フラグが立っていない場所に限定する
+        if (h >> h2) & 1 == 0 and (w >> w2) & 1 == 0 and graph[h2][w2] == "#":
+          black_cnt += 1
+    if black_cnt == K:
+      ans += 1
+
+print(ans)
