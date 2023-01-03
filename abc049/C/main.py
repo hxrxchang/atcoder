c3710db2b0582664e1d93f23ff50c317eba4af14
hxrxchang <mail@hxrxchang.dev>
2023-01-03 17:31:29 +0900

    solve C

--- abc049/C/main.py
+++ abc049/C/main.py
@@ -1,1 +1,19 @@
+S = input()
+N = len(S)
+dp = [False] * (N + 1)
+dp[0] = True
 
+for i in range(N + 1):
+  if i >= 5 and dp[i - 5] and S[i - 5:i] == "dream":
+    dp[i] = True
+  if i >= 7 and dp[i - 7] and S[i - 7:i] == "dreamer":
+    dp[i] = True
+  if i >= 5 and dp[i - 5] and S[i - 5:i] == "erase":
+    dp[i] = True
+  if i >= 6 and dp[i - 6] and S[i - 6:i] == "eraser":
+    dp[i] = True
+
+if dp[-1]:
+  print("YES")
+else:
+  print("NO")
