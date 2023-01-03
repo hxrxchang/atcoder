f9d505049e283deba7eb1cefeaf166986d18bc0c
hxrxchang <mail@hxrxchang.dev>
2023-01-03 18:18:54 +0900

    fix A

--- dp/A/main2.py
+++ dp/A/main2.py
@@ -3,24 +3,20 @@ sys.setrecursionlimit(10 ** 6)
 
 N = int(input())
 H = list(map(int, input().split()))
-INF = 2 ** 60
-
 dp = [-1] * N
+
+INF = 2 ** 60
 
 def rec(i):
   if i == 0:
     return 0
-
   if dp[i] != -1:
     return dp[i]
-
   result = INF
-
   if i - 1 >= 0:
     result = min(result, rec(i - 1) + abs(H[i] - H[i - 1]))
   if i - 2 >= 0:
     result = min(result, rec(i - 2) + abs(H[i] - H[i - 2]))
-
   dp[i] = result
   return result
 
