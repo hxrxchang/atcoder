c40e81e051a6c1381ea00384acbed09bbc993bcd
hxrxchang <mail@hxrxchang.dev>
2023-01-08 12:54:50 +0900

    solve C

--- abc048/C/main.py
+++ abc048/C/main.py
@@ -1,1 +1,15 @@
+N, x = map(int, input().split())
+A = list(map(int, input().split()))
 
+cnt = 0
+
+if A[0] > x:
+  cnt += A[0] - x
+  A[0] = x
+
+for i in range(N - 1):
+  if A[i] + A[i + 1] > x:
+    cnt += A[i] + A[i + 1] - x
+    A[i + 1] = x - A[i]
+
+print(cnt)
