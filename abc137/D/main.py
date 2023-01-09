ece9181a55e52658d1a637ddfad60b1d1af3cc5b
hxrxchang <mail@hxrxchang.dev>
2023-01-10 01:41:44 +0900

    solve D

--- abc137/D/main.py
+++ abc137/D/main.py
@@ -1,1 +1,22 @@
+from heapq import heappush, heappop
 
+N, M = map(int, input().split())
+AtoB = [[] for _ in range(M + 1)]
+
+for _ in range(N):
+  A, B = map(int, input().split())
+  if A > M:
+    continue
+  AtoB[A].append(B)
+
+ans = 0
+
+que = []
+
+for B in AtoB:
+  for b in B:
+    heappush(que, -b)
+  if que:
+    ans += abs(heappop(que))
+
+print(ans)
