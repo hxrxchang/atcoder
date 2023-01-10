0f0914c95921ebd070611e836afc5dd8f92a81f0
hxrxchang <mail@hxrxchang.dev>
2023-01-11 00:37:16 +0900

    解き直し

--- agc033/A/main.py
+++ agc033/A/main.py
@@ -1,32 +1,34 @@
 from collections import deque
 
 H, W = map(int, input().split())
-graph = [[0] * W for _ in range(H)]
-que = deque()
+dist = [[-1] * W for _ in range(H)]
 black_cnt = 0
+que = deque()
 
-for i in range(H):
-  s = input()
-  for j in range(W):
-    if s[j] == "#":
-      graph[i][j] = 1
-      que.append((i, j))
+for y in range(H):
+  inp = list(input())
+  for x in range(W):
+    if inp[x] == "#":
+      dist[y][x] = 0
+      que.append((y, x))
       black_cnt += 1
 
-ans = 0
+cnt = 0
 while black_cnt < H * W:
+  cnt += 1
   next_que = deque()
-  ans += 1
-  while len(que) > 0:
+  while que:
     black_pos = que.popleft()
     y, x = black_pos[0], black_pos[1]
-    for next_black_pos in [(y + 1, x), (y - 1, x), (y, x + 1), (y, x - 1)]:
-      y2, x2 = next_black_pos[0], next_black_pos[1]
-      if 0 <= y2 and y2 < H and 0 <= x2 and x2 < W and graph[y2][x2] == 0:
-        graph[y2][x2] = 1
-        next_que.append((y2, x2))
+    black_pos_dist = dist[y][x]
+    for next_pos in [(y + 1, x), (y - 1, x), (y, x + 1), (y, x - 1)]:
+      y2, x2 = next_pos[0], next_pos[1]
+      if 0 <= y2 and y2 < H and 0 <= x2 and x2 < W and dist[y2][x2] == -1:
+        dist[y2][x2] = black_pos_dist + 1
         black_cnt += 1
+        next_que.append((y2, x2))
+  # que を入れ替えることで、このn回目の操作で新たに黒に変わったマスを記録できる
   que = next_que
 
-print(ans)
+print(cnt)
 
