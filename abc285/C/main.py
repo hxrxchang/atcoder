16679daa818bcb14e51c7751b0da60e5ab5c61ed
hxrxchang <mail@hxrxchang.dev>
2023-01-21 02:14:19 +0900

    solve C

--- abc285/C/main.py
+++ abc285/C/main.py
@@ -1,1 +1,14 @@
+S = input()
+
+data = []
+for s in list(S):
+  data.append(ord(s) - 64)
+
+data.reverse()
+
+cnt = data[0]
+for i in range(1, len(data)):
+  cnt += 26 ** i * data[i]
+
+print(cnt)
 
