1768a3b010402663b4d7d950f155e2600152b2ee
hxrxchang <mail@hxrxchang.dev>
2023-01-21 03:09:21 +0900

    solve D

--- abc285/D/main.py
+++ abc285/D/main.py
@@ -1,1 +1,38 @@
+N = int(input())
+
+class UnionFind:
+  def __init__(self, n):
+    self.parents = {}
+
+  def root(self, item):
+    if self.parents.get(item) == None:
+      self.parents[item] = item
+      return item
+    if self.parents.get(item) == item:
+      return item
+    self.parents[item] = self.root(self.parents.get(item))
+    return self.parents[item]
+
+  def unit(self, a, b):
+    a = self.root(a)
+    b = self.root(b)
+    if a == b:
+      return
+    self.parents[a] = b
+
+  def is_same(self, a, b):
+    return self.root(a) == self.root(b)
+
+uf = UnionFind(N)
+
+for _ in range(N):
+  first, second = input().split()
+  print(uf.parents)
+  # 新たにa,bに辺を張るとき，すでにa,bが同じグループに属していれば，この操作で閉路ができる．
+  if uf.is_same(first, second):
+    print("No")
+    exit()
+  uf.unit(first, second)
+
+print("Yes")
 
