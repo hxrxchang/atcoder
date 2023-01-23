07422406dfbbfa60763bd613e0b8ef7a8585842d
hxrxchang <mail@hxrxchang.dev>
2023-01-24 01:30:27 +0900

    今度こそAtCounter理解した

--- typical90/008/main.py
+++ typical90/008/main.py
@@ -1,28 +1,35 @@
 N = int(input())
 S = input()
 atcoder = "atcoder"
-division_number = 10 ** 9 + 7
 
-# dp[i][j]: Sのi文字目までを用いた時、文字列"atcoder"のj文字目までを作る通りの数
-dp = []
-for _ in range(N + 1):
-  row = [0] * (len(atcoder) + 1)
-  dp.append(row)
+# # 簡易データで考えるとわかりやすい
+# N = 6
+# S = "aatctc"
+# target_letter = "atc"
 
-# j=0 つまり文字を取り出さない方法は1通りのため
-for i in range(N + 1):
-  dp[i][0] = 1
+# dp[i][j] 文字列Sの最初のi文字から何文字か抜き出してつなげて、"atcoder"のj文字目までを作れる数
+# S="aatctc" だとしたら、dp[4][1]は "aatct" から "at" を作るパターンは何通りあるか？ということ (=4)
+dp = [[0] * len(atcoder) for _ in range(N)]
 
+# dp[i][0] つまり a が何種類あるかを先に計算
 for i in range(N):
-  for j in range(len(atcoder)):
-    if S[i] == atcoder[j]:
-      dp[i + 1][j + 1] = (dp[i][j] + dp[i][j + 1]) % division_number
+  if S[i] == atcoder[0]:
+    if i - 1 < 0:
+      dp[i][0] = 1
     else:
-      dp[i + 1][j + 1] = dp[i][j + 1]
+      dp[i][0] = dp[i - 1][0] + 1
+  else:
+    if i - 1 < 0:
+      dp[i][0] = 0
+    else:
+      dp[i][0] = dp[i - 1][0]
 
-print(dp[N][len(atcoder)])
+for i in range(1, N):
+  for j in range(1, len(atcoder)):
+    if S[i] == atcoder[j]:
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    else:
+      dp[i][j] = dp[i - 1][j]
+    dp[i][j] %= 10 ** 9 + 7
 
-# # ↓で確認するとわかりやすい
-# for i in range(len(dp)):
-#   print(S[i - 1], dp[i])
-
+print(dp[-1][-1])
