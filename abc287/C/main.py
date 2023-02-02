468a7bf141ab84e3f988cae8d67f8ba3c651d96e
hxrxchang <mail@hxrxchang.dev>
2023-02-02 19:16:12 +0900

    solve C

--- abc287/C/main.py
+++ abc287/C/main.py
@@ -1,15 +1,55 @@
-from collections import deque
-
 N, M = map(int, input().split())
+if M != N - 1:
+  print("No")
+  exit()
+
+class UnionFind:
+  def __init__(self, n):
+    # parentsは要素が正の値のときはそのインデックスのルートを表す。
+    # 負の値のときはそのインデックスはルートであり絶対値がそのルートが持つ要素数を表す。
+    self.parents = [-1] * n
+
+  def root(self, n):
+    if self.parents[n] < 0:
+      return n
+    self.parents[n] = self.root(self.parents[n])
+    return self.parents[n]
+
+  def unit(self, a, b):
+    a = self.root(a)
+    b = self.root(b)
+    if a == b:
+      return
+    # a, bはルートなので必ず負の値(そのルートがもつ要素数)になる
+    if abs(self.parents[a]) < abs(self.parents[b]):
+      b, a = a, b
+    # ルートの要素数を更新
+    self.parents[a] += self.parents[b]
+    # サイズが小さい方のルートを大きい方のルートに繋げる
+    self.parents[b] = a
+
+  def is_same(self, a, b):
+    return self.root(a) == self.root(b)
+
 graph = [[] for _ in range(N)]
+uf = UnionFind(N)
+for _ in range(M):
+  a, b = map(int, input().split())
+  a -= 1
+  b -= 1
+  graph[a].append(b)
+  graph[b].append(a)
+  uf.unit(a, b)
 
-for i in range(M):
-  A, B = map(int, input().split())
-  A -= 1
-  B -= 1
-  if len(graph[B]):
-    b = graph[B][0]
+for g in graph:
+  if len(g) > 2:
+    print("No")
+    exit()
 
-  graph[A].append(B)
-  graph[B].append(A)
+for i in range(N - 1):
+  if not uf.is_same(i, i + 1):
+    print("No")
+    exit()
+
+print("Yes")
 
