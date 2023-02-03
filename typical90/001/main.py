7775c780bc1d40c327c207a3e59f95d494b979b8
hxrxchang <mail@hxrxchang.dev>
2023-02-04 00:22:10 +0900

    羊羹パーティ理解

--- typical90/001/main.py
+++ typical90/001/main.py
@@ -2,9 +2,9 @@ N, L = map(int, input().split())
 K = int(input())
 A = [0] + list(map(int, input().split())) + [L]
 
-# checkを自力で実装したい
+# Aを左から順番に調べて、xより大きいものを取り出してその数を調べる
 def check(x):
-  left, right = 0, 0
+  left, right = 0, 1
   cnt = 0
   while right < len(A):
     if A[right] - A[left] >= x:
@@ -17,8 +17,8 @@ def check(x):
     return True
   return False
 
-# 1cm ~ L+1cmの間でもっとも短いものの長さが最大になるものを二分探索
-ok, ng = 1, L + 1
+# 1cm ~ Lcmの間でもっとも短いものの長さが最大になるものを二分探索
+ok, ng = 1, L
 while ng - ok > 1:
   mid = (ok + ng) // 2
   if check(mid):
