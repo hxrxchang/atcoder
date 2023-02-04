5ccf37c589b3d4eb0edd05f84e0d8e52a0368a69
hxrxchang <mail@hxrxchang.dev>
2023-02-05 01:12:57 +0900

    solve 002

--- typical90/002/main.py
+++ typical90/002/main.py
@@ -1,1 +1,26 @@
+N = int(input())
 
+def check(st):
+  if st.count("(") != st.count(")"):
+    return False
+  # stを先頭から一文字ずつチェックしていき、どのタイミングにおいても"("の数が")"を下回ることはない
+  left_cnt = 0
+  right_cnt = 0
+  for s in st:
+    if s == "(":
+      left_cnt += 1
+    elif s == ")":
+      right_cnt += 1
+    if left_cnt < right_cnt:
+      return False
+  return True
+
+for i in range(2 ** N):
+  st = ""
+  for j in range(N - 1, -1, -1):
+    if (i >> j) & 1:
+      st += ")"
+    else:
+      st += "("
+  if check(st):
+    print(st)
