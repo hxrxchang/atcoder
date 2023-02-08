f8b653c034f712c312dcfadcb5cf172e33c82b29
hxrxchang <mail@hxrxchang.dev>
2023-02-09 00:23:45 +0900

    solve 004

--- typical90/004/main.py
+++ typical90/004/main.py
@@ -1,1 +1,28 @@
+H, W = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(H)]
+
+rows = []
+for i in range(H):
+  v = 0
+  for j in range(W):
+    v += A[i][j]
+  rows.append(v)
+
+columns = []
+for i in range(W):
+  v = 0
+  for j in range(H):
+    v += A[j][i]
+  columns.append(v)
+
+for i in range(H):
+  ans = []
+  for j in range(W):
+    item = A[i][j]
+    val = rows[i] + columns[j]
+    # 縦と横両方に自分が含まれるから自分1つ分引く
+    val -= item
+    ans.append(val)
+  print(*ans)
+
 
