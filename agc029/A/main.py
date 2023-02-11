6e87d573bcce80c7f29b4996c763861c314b50f3
hxrxchang <mail@hxrxchang.dev>
2023-02-11 12:41:59 +0900

    solve A

--- agc029/A/main.py
+++ agc029/A/main.py
@@ -1,1 +1,16 @@
+# 似てそうな問題
+# https://atcoder.jp/contests/abc125/tasks/abc125_d
+# https://atcoder.jp/contests/abc174/tasks/abc174_d
 
+# 最終的に `BWBWBWBWB` が `WWWBBBB` になるようにすればいい
+# つまり入力で得た文字列の各Wの要素を調べて、そのWより前にいるBの数を調べて合計する
+S = input()
+cnt = 0
+b_cnt = 0
+for i in range(len(S)):
+  if S[i] == "W":
+    cnt += b_cnt
+  else:
+    b_cnt += 1
+
+print(cnt)
