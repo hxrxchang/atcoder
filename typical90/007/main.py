138e603ca1dd96000388cea94c1507af2b3f2d1e
hxrxchang <mail@hxrxchang.dev>
2023-02-16 23:48:45 +0900

    solve 007

--- typical90/007/main.py
+++ typical90/007/main.py
@@ -1,1 +1,24 @@
+N = int(input())
+A = list(map(int, input().split()))
+A.sort()
+Q = int(input())
 
+INF = 2 ** 60
+
+for i in range(Q):
+  B = int(input())
+  left = 0
+  right = N
+  while right - left > 1:
+    mid = (right + left) // 2
+    if A[mid] <= B:
+      left = mid
+    else:
+      right = mid
+
+  ans = INF
+  if right < N:
+    ans = min(ans, abs(B - A[right]))
+  if right > 0:
+    ans = min(ans, abs(B - A[left]))
+  print(ans)
