90f5c44e0cda402efd2c05a2e7cda18d6e505f53
hxrxchang <mail@hxrxchang.dev>
2023-03-04 20:58:36 +0900

    solve A

--- agc040/A/main.py
+++ agc040/A/main.py
@@ -1,4 +1,21 @@
 S = input()
+n = len(S) + 1
 
-for i, s in enumerate(S):
-  print(s)
+memo = [0] * n
+
+for i in range(n - 1):
+  s = S[i]
+  if s == "<":
+    memo[i + 1] = memo[i] + 1
+
+print(memo)
+
+for i in range(n - 2, -1, -1):
+  s = S[i]
+  if s == ">":
+    print(memo[i], memo[i + 1])
+    memo[i] = max(memo[i], memo[i + 1] + 1)
+
+ans = sum(memo)
+
+print(ans)
