c7bf8f01848db2d3d00f7711bc1ed88c8862596a
hxrxchang <mail@hxrxchang.dev>
2023-03-07 20:01:00 +0900

    solve 014

--- typical90/014/main.py
+++ typical90/014/main.py
@@ -1,1 +1,11 @@
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+A.sort()
+B.sort()
 
+cnt = 0
+for i in range(N):
+  cnt += abs(A[i] - B[i])
+
+print(cnt)
