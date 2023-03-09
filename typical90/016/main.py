b23ec8693a9525ec828e722011d19915cdf3e80c
hxrxchang <mail@hxrxchang.dev>
2023-03-09 09:50:03 +0900

    solve 016

--- typical90/016/main.py
+++ typical90/016/main.py
@@ -1,1 +1,13 @@
+N = int(input())
+coins = list(map(int, input().split()))
 
+ans = 10000
+for x in range(0, 10000):
+  for y in range(0, 10000):
+    diff = N - (coins[0] * x + coins[1] * y)
+    if diff >= 0 and diff % coins[2] == 0:
+      tmp = x + y + diff // coins[2]
+      if tmp < ans:
+        ans = tmp
+
+print(ans)
