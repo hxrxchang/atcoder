4e61170ab1f95f1193186d803932b87cbd78bfeb
hxrxchang <mail@hxrxchang.dev>
2023-03-11 01:09:07 +0900

    solve C

--- abc160/C/main.py
+++ abc160/C/main.py
@@ -1,1 +1,14 @@
+K, N = map(int, input().split())
+A = list(map(int, input().split()))
 
+longest = 0
+for i in range(0, N - 1):
+  if longest < A[i + 1] - A[i]:
+    longest = A[i + 1] - A[i]
+
+if longest < K - A[-1] + A[0]:
+  longest = K - A[-1] + A[0]
+
+ans = K - longest
+
+print(ans)
