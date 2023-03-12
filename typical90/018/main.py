f1696cc909e00ecd3728bb723c447268ab443583
hxrxchang <mail@hxrxchang.dev>
2023-03-12 14:55:21 +0900

    解説見てもわからない

--- typical90/018/main.py
+++ typical90/018/main.py
@@ -1,7 +1,16 @@
+from math import atan, cos, sin, pi
+
 T = int(input())
 L, X, Y = map(int, input().split())
 Q = int(input())
 
+def calc(t):
+  theta = 2 * pi * t / T
+  height = L / 2 - L / 2 * cos(theta)
+  y = -(L / 2 * sin(theta))
+  dist = (X * X + (y - Y) ** 2) ** 0.5
+  return atan(height / dist) / pi * 180
 
 for _ in range(Q):
-  E = int(input())
+  e = int(input())
+  print(calc(e))
