4404a2685027c67ddbebc777aca249e408440df8
hxrxchang <mail@hxrxchang.dev>
2023-03-18 23:59:38 +0900

    gen abc092

---
+++ abc092/B/main.py
@@ -0,0 +1,17 @@
+N = int(input())
+D, X = map(int, input().split())
+cnt = 0
+for _ in range(N):
+  A = int(input())
+  c = 0
+  i = 1
+  days = 1
+  while days <= D:
+    tmp +=
+    i += 1
+
+ans = D + cnt
+
+print(ans)
+
+
