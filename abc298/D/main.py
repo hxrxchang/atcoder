7ca8d5fe4fc587c2f724f969be86521a78ba32cc
hxrxchang <mail@hxrxchang.dev>
2023-04-16 02:39:40 +0900

    solve D

--- abc298/D/main.py
+++ abc298/D/main.py
@@ -1,14 +1,19 @@
-Q_ = int(input())
-S = [1]
-sum = '1'
+from collections import deque
 
+mod = 998244353
+Q_ = int(input())
+que = deque()
+ans = 1
+que.append(1)
 for _ in range(Q_):
     Q = list(map(int, input().split()))
     if Q[0] == 1:
-        S.append(Q[1])
-        sum += str(Q[1])
+        que.append(Q[1])
+        ans = (ans * 10 + Q[1]) % mod
     elif Q[0] == 2:
-        p = S.pop(0)
-        sum = sum[1:]
+        keta = len(que)
+        top_el = que.popleft()
+        ans -= top_el * pow(10, keta - 1, mod) % mod
+        ans %= mod
     elif Q[0] == 3:
-        print(int(sum) % 998244353)
+        print(ans)
