8a486596d7bf2c617d079feb9eb4ad90cbe8c8d1
hxrxchang <mail@hxrxchang.dev>
2023-04-18 20:04:53 +0900

    solve C

--- abc004/C/main.py
+++ abc004/C/main.py
@@ -1,1 +1,12 @@
+N = int(input())
+N %= 30
+A = ['1', '2', '3', '4', '5', '6']
 
+for i in range(N):
+    A[i % 5], A[i % 5 + 1] = A[i % 5 + 1], A[i % 5]
+
+ans = ""
+for a in A:
+    ans += a
+
+print(ans)
