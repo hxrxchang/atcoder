b64e11e6ef3b479e984699acf6f6f5422a086a80
hxrxchang <mail@hxrxchang.dev>
2023-04-26 01:09:56 +0900

    solve 026
    ref: https://logicalbear.net/%E3%80%90%E7%AB%B6%E3%83%97%E3%83%AD%E5%85%B8%E5%9E%8B90%E5%95%8F%E3%80%91%E3%80%8C008-atcounter%EF%BC%88%E2%98%854%EF%BC%89%E3%80%8D%E8%A7%A3%E6%B3%95-2/

--- typical90/026/main.py
+++ typical90/026/main.py
@@ -1,1 +1,37 @@
+import sys
+from collections import deque
+sys.setrecursionlimit(10 ** 9)
 
+N = int(input())
+
+nodes = [[] for _ in range(N + 1)]
+for _ in range(N - 1):
+    a, b = map(int, input().split())
+    nodes[a].append(b)
+    nodes[b].append(a)
+
+dist = [-1] * (N + 1)
+dist[1] = 0
+
+d_even = [1]
+d_odd = []
+
+que = deque()
+que.append(1)
+
+while que:
+    current = que.popleft()
+    next_nodes = nodes[current]
+    for node in next_nodes:
+        if dist[node] == -1:
+            dist[node] = dist[current] + 1
+            que.append(node)
+            if dist[node] % 2 == 0:
+                d_even.append(node)
+            else:
+                d_odd.append(node)
+
+if len(d_even) >= len(d_odd):
+    print(*d_even[:N // 2])
+else:
+    print(*d_odd[:N // 2])
