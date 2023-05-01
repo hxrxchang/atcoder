5a22ad036151b608b709aad1615d5877cd61c9b0
hxrxchang <mail@hxrxchang.dev>
2023-05-01 23:38:37 +0900

    fix B

--- abc300/B/main.py
+++ abc300/B/main.py
@@ -1,17 +1,18 @@
 H, W = map(int, input().split())
-A = [list(input()) for i in range(H)]
-B = [list(input()) for i in range(H)]
+A = [list(input()) for _ in range(H)]
+B = [list(input()) for _ in range(H)]
 
-# (s, t) の組み合わせを全探索
 for s in range(H):
     for t in range(W):
-        jugde = True
-        # A, B の各マスを全探索で比較
+        flag = True
         for i in range(H):
             for j in range(W):
-                if A[(i - s + H) % H][(j - t + W) % W] != B[i][j]:
-                    jugde = False
-        if jugde:
+                if A[(s + i) % H][(t + j) % W] != B[i][j]:
+                    flag = False
+                    break
+                if not flag:
+                    break
+        if flag:
             print("Yes")
             exit()
 
