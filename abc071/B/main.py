17b0a495b26e620e77c7582b454a4979d6fddd0b
hxrxchang <mail@hxrxchang.dev>
2023-05-05 00:56:22 +0900

    solve B

--- abc071/B/main.py
+++ abc071/B/main.py
@@ -1,1 +1,13 @@
+import string
 
+S = input()
+memo = set()
+for s in S:
+    memo.add(s)
+
+for s in string.ascii_lowercase:
+    if s not in memo:
+        print(s)
+        exit()
+
+print('None')
