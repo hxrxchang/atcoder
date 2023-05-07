cad8aff3a2b7c8b92cc7ea2e30ef2cbe5356d7e7
hxrxchang <mail@hxrxchang.dev>
2023-05-07 13:34:48 +0900

    solve 029 (Substack 1)
    https://atcoder.jp/contests/typical90/submissions/41223439

--- typical90/029/main.py
+++ typical90/029/main.py
@@ -1,1 +1,9 @@
+W, N = map(int, input().split())
+H = [0] * (W + 1)
 
+for _ in range(N):
+    L, R = map(int, input().split())
+    maximum = max(H[L:R + 1])
+    for i in range(L, R + 1):
+        H[i] = maximum + 1
+    print(maximum + 1)
