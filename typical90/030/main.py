b0bbe93909a69a5059b80a91d576bf3d7d2b5357
hxrxchang <mail@hxrxchang.dev>
2023-05-07 22:54:14 +0900

    solve 030

--- typical90/030/main.py
+++ typical90/030/main.py
@@ -1,1 +1,17 @@
+N, K = map(int, input().split())
 
+cnt = [0] * (N + 1)
+
+# エラトステネスの篩と同じように、初めて出現した素数の倍数については、その数を素因数に持つ数としてカウントする。
+for i in range(2, N + 1):
+    if cnt[i] > 0:
+        continue
+    for j in range(i, N + 1, i):
+        cnt[j] += 1
+
+ans = 0
+for i in range(1, N + 1):
+    if cnt[i] >= K:
+        ans += 1
+
+print(ans)
