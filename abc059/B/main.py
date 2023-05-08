c0e27f1eb7507927dac45d85f3ada24d83c86b4c
hxrxchang <mail@hxrxchang.dev>
2023-05-08 09:09:26 +0900

    solve B

---
+++ abc059/B/main.py
@@ -0,0 +1,18 @@
+A = input()
+B = input()
+
+if A == B:
+    print("EQUAL")
+elif len(A) > len(B):
+    print("GREATER")
+elif len(B) > len(A):
+    print("LESS")
+else:
+    numbers = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+    for i in range(len(A)):
+        if int(A[i]) > int(B[i]):
+            print("GREATER")
+            break
+        elif int(B[i]) > int(A[i]):
+            print("LESS")
+            break
