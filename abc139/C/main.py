9eb6192d41c9451b846277560ead093ecbe30444
hxrxchang <mail@hxrxchang.dev>
2023-05-09 07:53:44 +0900

    solve C

--- abc139/C/main.py
+++ abc139/C/main.py
@@ -1,1 +1,15 @@
+N = int(input())
+H = list(map(int, input().split()))
+
+cnt = 0
+tmp_cnt = 0
+for i in range(1, N):
+    if H[i] <= H[i - 1]:
+        tmp_cnt += 1
+    else:
+        cnt = max(cnt, tmp_cnt)
+        tmp_cnt = 0
+
+cnt = max(cnt, tmp_cnt)
+print(cnt)
 
