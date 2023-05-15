2549e8d09db4edc6f4ede89c0be5768e7de50e1f
hxrxchang <mail@hxrxchang.dev>
2023-05-15 23:58:47 +0900

    solve B

---
+++ abc050/B/main.py
@@ -0,0 +1,9 @@
+N = int(input())
+T = list(map(int, input().split()))
+sumT = sum(T)
+M = int(input())
+for _ in range(M):
+  P, X = map(int, input().split())
+  ans = sumT - T[P - 1] + X
+  print(ans)
+
