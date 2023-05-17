06f795888013b9ee7e9178219913317bcd29523f
hxrxchang <mail@hxrxchang.dev>
2023-05-17 12:32:52 +0900

    解き直し

--- abc289/D/main.py
+++ abc289/D/main.py
@@ -1,25 +1,29 @@
 N = int(input())
 A = list(map(int, input().split()))
+
 M = int(input())
 B = list(map(int, input().split()))
+
 X = int(input())
 
-# dp[i]段目に False: 登れない、True: 登れる
+# dp[i]: i段目に登れるかどうか
 dp = [False] * (X + 1)
-# 0段目には登れる
 dp[0] = True
 
-# M大きいので探索不要になるように全indexの結果を保存しておく
-available = [True] * (X + 1)
+# モチがあるかをO1で判定するためにメモ
+memo_B = [False] * (X + 1)
 for b in B:
-  available[b] = False
+  memo_B[b] = True
 
 for i in range(1, X + 1):
-  if not available[i]:
+  # モチがあるのであれば、その段には登れない
+  if memo_B[i]:
     continue
   for a in A:
-    # iのa段前がTrueなら、iにたどり着ける
     if i >= a and dp[i - a]:
       dp[i] = True
 
-print("Yes" if dp[X] else "No")
+if dp[X]:
+  print("Yes")
+else:
+  print("No")
