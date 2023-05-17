a9db939daa466e0def98b221bd036b74e8445c7f
hxrxchang <mail@hxrxchang.dev>
2023-05-17 12:14:37 +0900

    解き直し

--- abc287/D/main.py
+++ abc287/D/main.py
@@ -2,33 +2,31 @@ S = list(input())
 T = list(input())
 
 def match(x, y):
-  if x == y or x == "?" or y == "?":
-    return True
-  return False
+  return x == y or x == '?' or y == '?'
 
-# S, Tの先頭からi文字がマッチするかどうか
-pre = [False] * (len(T) + 1)
-pre[0] = True #先頭からゼロ文字 = 何も選ばない なのでTrue
-for i in range(len(T)):
-  if not match(S[i], T[i]):
-    break
-  pre[i + 1] = True
+# pre[i] = S[:i] と T[:i] が一致するか
+pre = [False] * (len(S) + 1)
+pre[0] = True
 
-# S, Tの末尾からi文字がマッチするかどうか
-suf = [False] * (len(T) + 1)
-suf[0] = True #末尾からゼロ文字 = 何も選ばない なのでTrue
-S.reverse()
-T.reverse()
 for i in range(len(T)):
-  if not match(S[i], T[i]):
+  if match(S[i], T[i]):
+    pre[i + 1] = True
+  else:
     break
-  suf[i + 1] = True
+
+S_ = list(reversed(S))
+T_ = list(reversed(T))
+# suf[i] = S[-i:] と T[-i:] が一致するか
+suf = [False] * (len(S) + 1)
+suf[0] = True
+for i in range(len(T)):
+  if match(S_[i], T_[i]):
+    suf[i + 1] = True
+  else:
+    break
 
 for i in range(len(T) + 1):
-  # ex: len(T): 2 だとする
-  # i = 0 のときは pre[0], suf[2]
-  # i = 1 のときは pre[1], suf[1] になるようにしたい
   if pre[i] and suf[len(T) - i]:
-    print("Yes")
+    print('Yes')
   else:
-    print("No")
+    print('No')
