edf8dc47e7ead94a4548d58e7a758c7155fac80b
hxrxchang <mail@hxrxchang.dev>
2023-05-19 01:03:53 +0900

    solve C

--- abc286/C/main.py
+++ abc286/C/main.py
@@ -1,6 +1,15 @@
+import math
+
 N, A, B = map(int, input().split())
 S = input()
 
+ans = float('inf')
+for i in range(N):
+  S_ = S[i:] + S[:i]
+  tmp = i * A
+  for j in range(math.ceil(N / 2)):
+    if S_[j] != S_[-1-j]:
+      tmp += B
+  ans = min(ans, tmp)
 
-
-
+print(ans)
