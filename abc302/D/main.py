0fbe547c63089b7bfe0e10e281cea2d3a5c72f74
hxrxchang <mail@hxrxchang.dev>
2023-05-21 13:48:30 +0900

    solve D

--- abc302/D/main.py
+++ abc302/D/main.py
@@ -1,1 +1,18 @@
+N, M, D = map(int, input().split())
+A = sorted(list(map(int, input().split())))
+B = sorted(list(map(int, input().split())))
 
+i = N - 1
+j = M - 1
+
+while (i >= 0 and j >= 0):
+  if abs(A[i] - B[j]) <= D:
+    print(A[i] + B[j])
+    exit()
+  else:
+    if A[i] > B[j]:
+      i -= 1
+    else:
+      j -= 1
+
+print(-1)
