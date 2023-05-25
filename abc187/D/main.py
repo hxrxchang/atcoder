63652ae8fce51f73978b98d00bf30bf161ae373c
hxrxchang <mail@hxrxchang.dev>
2023-05-26 01:34:32 +0900

    solve D

--- abc187/D/main.py
+++ abc187/D/main.py
@@ -1,17 +1,18 @@
 N = int(input())
-towns = []
-aoki_sum = 0
+# 高橋くんの青木くんとの獲得票数の差。高橋くんが勝っている場合は正の値、青木くんが勝っている場合は負の値
+point_diff = 0
+towns_vote_data = []
+
 for _ in range(N):
   A, B = map(int, input().split())
-  towns.append((A, B, A + B))
-  aoki_sum += A
+  towns_vote_data.append([A, B, 2 * A + B])
+  point_diff -= A
 
-towns.sort(key=lambda x: x[2], reverse=True)
+towns_vote_data.sort(key=lambda x: x[2], reverse=True)
 
-takahashi_sum = 0
-for i, t in enumerate(towns):
-  takahashi_sum += t[2]
-  aoki_sum -= t[0]
-  if takahashi_sum > aoki_sum:
+for i, town in enumerate(towns_vote_data):
+  # 高橋くんはA, B両方の票を獲得することができ、青木くんはAの票数を失うので、town[0] * 2 する。
+  point_diff += town[0] * 2 + town[1]
+  if point_diff > 0:
     print(i + 1)
     break
