0706d6c55e60b6fcc7c75dc91f82129769868176
hxrxchang <mail@hxrxchang.dev>
2023-06-03 01:19:22 +0900

    solve C

--- abc121/C/main.py
+++ abc121/C/main.py
@@ -1,1 +1,22 @@
+N, M = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(N)]
 
+A.sort(key=lambda x: x[0])
+
+cnt_amount = 0
+cnt_count = 0
+
+for a in A:
+  money_per_count = a[0]
+  purchase_max = a[1]
+  if cnt_count + purchase_max <= M:
+    cnt_amount += money_per_count * purchase_max
+    cnt_count += purchase_max
+    if cnt_count == M:
+      break
+  else:
+    cnt_amount += money_per_count * (M - cnt_count)
+    cnt_count += M - cnt_count
+    break
+
+print(cnt_amount)
