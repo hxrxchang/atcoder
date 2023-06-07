9e115fd7e3506e355fa2dff8f453005bad420159
hxrxchang <mail@hxrxchang.dev>
2023-06-07 09:59:58 +0900

    temp

--- abc093/C/main.py
+++ abc093/C/main.py
@@ -1,1 +1,17 @@
+numbers = list(map(int, input().split()))
 
+cnt = 0
+while True:
+  numbers.sort()
+  A, B, C = numbers[0], numbers[1], numbers[2]
+  if C - A == 1 and C - B == 1:
+    cnt += 1
+    break
+  elif C - A == 2 and B == C:
+    cnt += 1
+    break
+  else:
+    numbers[0] += 2
+    cnt += 1
+
+print(cnt)
--- typical90/034/main.py
+++ typical90/034/main.py
@@ -1,1 +1,27 @@
+from collections import defaultdict
 
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+
+cnt = defaultdict(int)
+elements = 0
+ans = 0
+right = 0
+
+for left in range(N):
+  while right < N and elements <= K:
+    cnt[A[right]] += 1
+    elements += 1
+    right += 1
+
+  if elements <= K:
+    ans = max(ans, right - left)
+  else:
+    ans = max(ans, right - left - 1)
+
+  cnt[A[left]] -= 1
+  if cnt[A[left]] == 0:
+    elements -= 1
+    del cnt[A[left]]
+
+print(ans)
