2cdc464c7e85d2b7758713cf6b0b19f20da7a477
hxrxchang <mail@hxrxchang.dev>
2023-06-08 01:08:55 +0900

    solve 034

--- typical90/034/main.py
+++ typical90/034/main.py
@@ -4,24 +4,24 @@ N, K = map(int, input().split())
 A = list(map(int, input().split()))
 
 cnt = defaultdict(int)
-elements = 0
-ans = 0
 right = 0
+ans = 0
+tmp_elements = 0
 
 for left in range(N):
-  while right < N and elements <= K:
+  while right < N and tmp_elements <= K:
+    if cnt[A[right]] == 0:
+      tmp_elements += 1
     cnt[A[right]] += 1
-    elements += 1
     right += 1
 
-  if elements <= K:
+  if tmp_elements <= K:
     ans = max(ans, right - left)
   else:
     ans = max(ans, right - left - 1)
 
-  cnt[A[left]] -= 1
-  if cnt[A[left]] == 0:
-    elements -= 1
-    del cnt[A[left]]
+    cnt[A[left]] -= 1
+    if cnt[A[left]] == 0:
+      tmp_elements -= 1
 
 print(ans)
