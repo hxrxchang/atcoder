0b361c6a9e68e7a225bc00b01f13efcccc13288f
hxrxchang <mail@hxrxchang.dev>
2023-06-15 22:00:49 +0900

    tmp

--- abc293/C/main.py
+++ abc293/C/main.py
@@ -1,29 +1,15 @@
-import sys
-sys.setrecursionlimit(10 ** 6)
 from collections import deque
+from copy
 
 H, W = map(int, input().split())
-graph = []
-for _ in range(H):
-  row = list(map(int, input().split()))
-  graph.append(row)
 
-dist = [[None] * W for _ in range(H)]
-dist[0][0] = 0
+A = [list(map(int, input().split())) for _ in range(H)]
 
 que = deque()
-que.append((0, 0))
-tmp = set()
-tmp.add(graph[0][0])
-cnt = 0
-while len(que):
-  y, x = que.popleft()
-  for y2, x2 in [(y + 1, x), (y, x + 1)]:
-    if y2 < H and x2 < W:
-      if not graph[y2][x2] in tmp:
-        tmp.add(graph[y2][x2])
-        que.append((y2, x2))
-  if y == H - 1 and x == W - 1:
-    cnt += 1
+que.append((1, 0))
+que.append((0, 1))
+pattern = [(0, 0)]
 
-print(cnt)
+
+
+
