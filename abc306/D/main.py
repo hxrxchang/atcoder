12863217614cbe67662dad9efd9863151f8fa607
hxrxchang <mail@hxrxchang.dev>
2023-06-18 00:54:19 +0900

    solve D

--- abc306/D/main.py
+++ abc306/D/main.py
@@ -1,18 +1,16 @@
 N = int(input())
 dishes = [tuple(map(int, input().split())) for _ in range(N)]
 
-dp = [[0, 0] for _ in range(N+1)]  # DPテーブルの初期
-for i in range(1, N+1):
-  dish = dishes[i-1]
-  if dish[0] == 1:
-    if i != 0 and dp[i - 1][0] == dp[i - 1][1]:
-      dp[i][0] = dp[i-1][0]
-      dp[i][1] = dp[i-1][1]
-    else:
-      dp[i][0] = max(dp[i-1][0] + dish[1], dp[i - 1][0])
-      dp[i][1] = dp[i-1][1]
+# dp[i][0] -> i番目までの皿の中から選んで、お腹を壊していない状態での、美味しさの最大値
+# dp[i][1] -> i番目までの皿の中から選んで、お腹を壊した状態での、美味しさの最大値
+dp = [[0, 0] for _ in range(N + 1)]
+for i in range(1, N + 1):
+  dish = dishes[i - 1]
+  if dish[0] == 0:
+    dp[i][0] = max(dp[i - 1][0], max(dp[i - 1][0], dp[i - 1][1]) + dish[1])
   else:
-    dp[i][0] = max(dp[i-1][0] + dish[1], dp[i - 1][0])
-    dp[i][1] = max(dp[i-1][1] + dish[1], dp[i - 1][1])
+    dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + dish[1])
+  dp[i][0] = max(dp[i - 1][0], dp[i][0])
+  dp[i][1] = max(dp[i - 1][1], dp[i][1])
 
-print(dp[-1])
+print(max(dp[N]))
