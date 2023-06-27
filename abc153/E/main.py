d2add6dbd8d6f00dfbd6840f220538ff8d893cd6
hxrxchang <mail@hxrxchang.dev>
2023-06-27 21:45:37 +0900

    solve E

--- abc153/E/main.py
+++ abc153/E/main.py
@@ -1,23 +1,16 @@
+INF = float('inf')
+
 H, N = map(int, input().split())
-magics = []
-for _ in range(N):
-  A, B = map(int, input().split())
-  C = A / B
-  magics.append((A, B, C))
+magics = [tuple(map(int, input().split())) for _ in range(N)]
 
-magics.sort(key=lambda x: (-x[2], x[1]))
+# dp[i][j]: i番目までの魔法を使ってモンスターの体力をjにするのに必要な最小のコスト
+dp = [[INF] * (H + 1) for _ in range(N + 1)]
+dp[0][0] = 0
 
-cnt = 0
+for i in range(N):
+  for j in range(H + 1):
+    dp[i + 1][j] = min(dp[i + 1][j], dp[i][j])
+    a, b = magics[i][0], magics[i][1]
+    dp[i + 1][min(j + a, H)] = min(dp[i + 1][min(j + a, H)], dp[i + 1][j] + b)
 
-for i, m in enumerate(magics):
-  if H <= m[0]:
-    cnt += m[1]
-    break
-  else:
-    d = H // m[0]
-    cnt += d * m[1]
-    H = H % m[0]
-    if H == 0:
-      break
-
-print(cnt)
+print(dp[N][H])
