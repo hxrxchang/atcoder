b4433c246704d4fdfb859fe469cf81d219418d79
hxrxchang <mail@hxrxchang.dev>
2023-06-28 03:14:38 +0900

    解き直し

--- abc153/E/main.py
+++ abc153/E/main.py
@@ -3,14 +3,13 @@ INF = float('inf')
 H, N = map(int, input().split())
 magics = [tuple(map(int, input().split())) for _ in range(N)]
 
-# dp[i][j]: i番目までの魔法を使ってモンスターの体力をjにするのに必要な最小のコスト
+# dp[i][j]: i番目までの魔法を使って(何回使ってもいい)モンスターの体力をj以上消費するにするのに必要な最小のコスト
 dp = [[INF] * (H + 1) for _ in range(N + 1)]
 dp[0][0] = 0
 
 for i in range(N):
   for j in range(H + 1):
-    dp[i + 1][j] = min(dp[i + 1][j], dp[i][j])
-    a, b = magics[i][0], magics[i][1]
-    dp[i + 1][min(j + a, H)] = min(dp[i + 1][min(j + a, H)], dp[i + 1][j] + b)
+    a, b = magics[i]
+    dp[i + 1][j] = min(dp[i][j], dp[i + 1][max(0, j - a)] + b) # max(0, j - a)はj - aが負の場合に対応するため
 
 print(dp[N][H])
