eed59f3d3b1c845a7746bd02d20d6266c22f23c0
hxrxchang <mail@hxrxchang.dev>
2023-07-21 01:22:37 +0900

    solve 042

--- typical90/042/main.py
+++ typical90/042/main.py
@@ -1,1 +1,16 @@
+K = int(input())
 
+if K % 9 != 0:
+  print(0)
+  exit()
+
+dp = [0] * (K + 1)
+dp[0] = 1
+
+for i in range(1, K + 1):
+  for j in range(1, min(i, 9) + 1):
+    dp[i] += dp[i - j]
+    dp[i] %= 10 ** 9 + 7
+
+print(dp[K])
+print(dp)
