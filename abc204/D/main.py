8fc47ad497784e1f5f593bd7b72079c28bcda0f2
hxrxchang <mail@hxrxchang.dev>
2023-08-01 23:01:13 +0900

    refactor

--- abc204/D/main.py
+++ abc204/D/main.py
@@ -1,7 +1,10 @@
+import math
+
 N = int(input())
 T = list(map(int, input().split()))
 sum_T = sum(T)
 
+# 方針: 最小値は N / 2 以上であることがわかっているので、N / 2以上合計以下の時間で作れる時間の最小値を求める
 # dp[i][j]: i番目までの料理で時間jを作れるか
 dp = [[False] * (sum_T + 1) for i in range(N + 1)]
 dp[0][0] = True
@@ -13,7 +16,7 @@ for i in range(1, N + 1):
       dp[i][j] = True
 
 # 全種類作った状態で、最小の時間を求める
-start = sum_T - sum_T // 2
+start = math.ceil(sum_T / 2)
 for i in range(start, sum_T + 1):
   if dp[N][i]:
     print(i)
