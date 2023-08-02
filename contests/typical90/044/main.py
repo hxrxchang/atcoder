e243d7a33b638add0ec1a0d8dd49725782dfc6ed
hxrxchang <mail@hxrxchang.dev>
2023-08-03 01:51:09 +0900

    solve 044

--- contests/typical90/044/main.py
+++ contests/typical90/044/main.py
@@ -1,1 +1,17 @@
+N, Q = map(int, input().split())
+A = list(map(int, input().split()))
 
+diff = 0
+for _ in range(Q):
+  T, X, Y = map(int, input().split())
+  X, Y = X - 1, Y - 1
+  if T == 1:
+    X = (X - diff)
+    Y = (Y - diff)
+    A[X], A[Y] = A[Y], A[X]
+  if T == 2:
+    diff += 1
+    diff %= N
+  if T == 3:
+    X = X - diff
+    print(A[X])
