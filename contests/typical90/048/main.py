549fd9529808669faed6190973421a597e82d8e9
hxrxchang <mail@hxrxchang.dev>
2023-08-30 20:00:04 +0900

    solve 048

--- contests/typical90/048/main.py
+++ contests/typical90/048/main.py
@@ -1,1 +1,11 @@
+N, K = map(int, input().split())
 
+scores = []
+for _ in range(N):
+  a, b = map(int, input().split())
+  scores.append(b)
+  scores.append(a - b)
+
+scores.sort(reverse=True)
+
+print(sum(scores[:K]))
