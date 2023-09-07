f2a79ad083a86cd45d214830fc3dd5ba676552da
hxrxchang <mail@hxrxchang.dev>
2023-09-08 00:06:25 +0900

    solve D

--- contests/abc161/D/main.py
+++ contests/abc161/D/main.py
@@ -1,1 +1,23 @@
+import heapq
+
+K = int(input())
+heap = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+heapq.heapify(heap)
+
+numbers = []
+while True:
+  item = heapq.heappop(heap)
+  if item > 3234566667:
+    break
+  numbers.append(item)
+
+  r = item % 10
+  heapq.heappush(heap, item * 10 + r)
+  if r != 0:
+    heapq.heappush(heap, item * 10 + r - 1)
+  if r != 9:
+    heapq.heappush(heap, item * 10 + r + 1)
+
+print(numbers[K - 1])
+
 
