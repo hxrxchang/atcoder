e48dc2932da95923bbf6fbfb088f28a561c3cb9a
hxrxchang <mail@hxrxchang.dev>
2023-09-07 21:57:29 +0900

    solve 050

--- contests/typical90/050/main.py
+++ contests/typical90/050/main.py
@@ -1,1 +1,12 @@
+N, L = map(int, input().split())
 
+dp = [0] * (N + 1)
+dp[0] = 1
+
+for i in range(1, N + 1):
+  dp[i] = dp[i - 1]
+  if i - L >= 0:
+    dp[i] += dp[i - L]
+  dp[i] = dp[i] % (10 ** 9 + 7)
+
+print(dp[N])
