a1f85ebe6b1878e42d6bb9323bda8f71e4c50da1
hxrxchang <mail@hxrxchang.dev>
2023-10-09 02:42:52 +0900

    solve D

--- contests/abc323/D/main.py
+++ contests/abc323/D/main.py
@@ -5,28 +5,20 @@ N = int(input())
 heap = []
 heapify(heap)
 slimes = defaultdict(int)
-slimes_set = set()
 
 for _ in range(N):
   s, c = map(int, input().split())
   slimes[s] = c
   heappush(heap, s)
-  slimes_set.add(s)
 
 while heap:
   s = heappop(heap)
-  slimes_set.remove(s)
   c = slimes[s]
-  if c < 2:
-    continue
-  else:
+  if c >= 2:
     monster_cnt = c // 2
     slimes[s * 2] += monster_cnt
+    heappush(heap, s * 2)
     slimes[s] -= 2 * monster_cnt
-    if s * 2 not in slimes_set:
-      heappush(heap, s * 2)
-      slimes_set.add(s * 2)
 
 ans = sum(slimes.values())
-
 print(ans)
