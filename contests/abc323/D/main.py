d2f7434891fd716f5bee4229addbf5809499024c
hxrxchang <mail@hxrxchang.dev>
2023-10-09 02:21:47 +0900

    solve D but TLE

--- contests/abc323/D/main.py
+++ contests/abc323/D/main.py
@@ -1,7 +1,32 @@
-import math
+from heapq import heappush, heappop, heapify
+from collections import defaultdict
 
 N = int(input())
-slimes = [tuple(map(int, input().split())) for _ in range(N)]
+heap = []
+heapify(heap)
+slimes = defaultdict(int)
+slimes_set = set()
 
-while True
-  if len(list(filter(labmda a: a[1] != 0), slimes)) != 0:
+for _ in range(N):
+  s, c = map(int, input().split())
+  slimes[s] = c
+  heappush(heap, s)
+  slimes_set.add(s)
+
+while heap:
+  s = heappop(heap)
+  slimes_set.remove(s)
+  c = slimes[s]
+  if c < 2:
+    continue
+  else:
+    monster_cnt = c // 2
+    slimes[s * 2] += monster_cnt
+    slimes[s] -= 2 * monster_cnt
+    if s * 2 not in slimes_set:
+      heappush(heap, s * 2)
+      slimes_set.add(s * 2)
+
+ans = sum(slimes.values())
+
+print(ans)
