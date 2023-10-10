62c0ac8a4053f6f9484f4a9824da6ae22d46d632
hxrxchang <mail@hxrxchang.dev>
2023-10-11 00:51:32 +0900

    solve 055

--- contests/typical90/055/main.py
+++ contests/typical90/055/main.py
@@ -1,1 +1,11 @@
+from itertools import combinations
 
+N, P, Q = map(int, input().split())
+A = list(map(int, input().split()))
+
+cnt = 0
+for comb in combinations(A, 5):
+  if comb[0] % P * comb[1] % P * comb[2] % P * comb[3] % P * comb[4] % P== Q:
+    cnt += 1
+
+print(cnt)
