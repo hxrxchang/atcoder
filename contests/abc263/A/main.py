a128d0903b4aba9c26571c58e8b92f361fecf87e
hxrxchang <mail@hxrxchang.dev>
2023-10-14 12:01:15 +0900

    solve A

--- contests/abc263/A/main.py
+++ contests/abc263/A/main.py
@@ -1,1 +1,18 @@
+from collections import defaultdict
 
+A = list(map(int, input().split()))
+
+d = defaultdict(int)
+
+for a in A:
+  d[a] += 1
+
+card_keys = list(d.keys())
+
+if len(card_keys) != 2:
+  print("No")
+else:
+  if d[card_keys[0]] == 2 or d[card_keys[1]] == 2:
+    print("Yes")
+  else:
+    print("No")
