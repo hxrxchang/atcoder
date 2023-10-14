d4e55b5dd11cc086d6e7f54ab3c6e65aaa51c92f
hxrxchang <mail@hxrxchang.dev>
2023-10-14 12:55:02 +0900

    solve B

--- contests/abc263/B/main.py
+++ contests/abc263/B/main.py
@@ -1,1 +1,18 @@
+from collections import deque
 
+N = int(input())
+P = list(map(int, input().split()))
+
+que = deque()
+dist = [-1] * (N + 1)
+que.append(N)
+dist[N] = 0
+
+while(que):
+  v = que.popleft()
+  p = P[v - 2]
+  if dist[p] == -1:
+    que.append(p)
+    dist[p] = dist[v] + 1
+
+print(dist[1])
