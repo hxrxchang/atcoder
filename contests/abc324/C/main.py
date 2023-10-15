d454492a09704b69d1000521562ad4bf07b76525
hxrxchang <mail@hxrxchang.dev>
2023-10-15 23:55:17 +0900

    refactor C

--- contests/abc324/C/main.py
+++ contests/abc324/C/main.py
@@ -1,67 +1,59 @@
 N, T = list(input().split())
 N = int(N)
-len_T = len(T)
 S = [input() for _ in range(N)]
 
 ans = []
-for j, s in enumerate(S):
-  # パターン1 同じ
-  if s == T:
-    ans.append(j + 1)
+
+def check(S, T):
+  # パターン1: 同じ
+  if S == T:
+    return True
   else:
-    len_s = len(s)
-    if abs(len_s - len_T) > 1:
-      continue
-    # パターン2 挿入
-    if len_T - len_s == 1:
-      flag = True
+    if abs(len(S) - len(T)) > 1:
+      return False
+    # パターン2: 挿入
+    elif len(T) - len(S) == 1:
       cnt = 0
-      for i in range(len_s):
-        if T[i + cnt] != s[i]:
-          if i + cnt + 1 < len_T and T[i + cnt + 1] == s[i]:
+      for i in range(len(S)):
+        if T[i + cnt] != S[i]:
+          if i + cnt + 1 < len(T) and T[i + cnt + 1] == S[i]:
             if cnt == 0:
               cnt += 1
               continue
             else:
-              flag = False
-              break
+              return False
           else:
-            flag = False
-            break
-      if flag:
-        ans.append(j + 1)
-    # パターン3 削除
-    elif len_s - len_T == 1:
-      flag = True
+            return False
+      return True
+    # パターン3: 削除
+    elif len(S) - len(T) == 1:
       cnt = 0
-      for i in range(len_T):
-        if s[i + cnt] != T[i]:
-          if i + cnt + 1 < len_s and s[i + cnt + 1] == T[i]:
+      for i in range(len(T)):
+        if S[i + cnt] != T[i]:
+          if i + cnt + 1 < len(S) and S[i + cnt + 1] == T[i]:
             if cnt == 0:
               cnt += 1
               continue
             else:
-              flag = False
-              break
+              return False
           else:
-            flag = False
-            break
-      if flag:
-        ans.append(j + 1)
-    # パターン4 置換
-    elif len_s == len_T:
-      flag = True
+            return False
+      return True
+    # パターン4: 置換
+    else:
       cnt = 0
-      for i in range(len_s):
-        if s[i] != T[i]:
+      for i in range(len(S)):
+        if S[i] != T[i]:
           if cnt == 0:
             cnt += 1
             continue
           else:
-            flag = False
-            break
-      if flag:
-        ans.append(j + 1)
+            return False
+      return True
+
+for i, s in enumerate(S):
+  if check(s, T):
+    ans.append(i + 1)
 
 print(len(ans))
 print(*ans)
