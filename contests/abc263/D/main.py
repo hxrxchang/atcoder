af8f3012d56eefc5d83073e4eddb198cdac0fa6d
hxrxchang <mail@hxrxchang.dev>
2023-10-17 00:45:27 +0900

    solve D

--- contests/abc263/D/main.py
+++ contests/abc263/D/main.py
@@ -1,17 +1,19 @@
 N, L, R = map(int, input().split())
 A = list(map(int, input().split()))
 
-sum_A = sum(A)
-dp = [sum_A] * N
+# i番目をLにするとき、i以降の数列の和の最小値
+dpL = [0] * N
+# i番目にA[i]を選ぶとき、i以降の数列の和の最小値
+dpA = [0] * N
+# i番目をRにするとき、i以降の数列の和の最小値
+dpR = [R * (N - i) for i in range(N)]
 
-left = 0
-right = N - 1
+dpA[-1] = A[-1]
+dpL[-1] = L
 
-if L < A[left]:
-  dp[0] = dp[0] - A[left] + L
-  left += 1
+for i in range(N - 1, 0, -1):
+  dpA[i - 1] = A[i - 1] + min(dpA[i], dpR[i])
+  dpL[i - 1] = L + min(dpL[i], dpA[i], dpR[i])
 
-if R < A[right]:
-  dp[0] = dp[0] - A[right] + R
-  right -= 1
-
+ans = min(dpA[0], dpL[0], dpR[0])
+print(ans)
