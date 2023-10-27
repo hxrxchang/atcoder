1d4866ae9d28c5c9de38cf64ed44cab4646c1984
hxrxchang <mail@hxrxchang.dev>
2023-10-28 01:19:40 +0900

    solve C

--- contests/sumitrust2019/C/main.py
+++ contests/sumitrust2019/C/main.py
@@ -1,1 +1,17 @@
+X = int(input())
 
+dp = [False] * (X + 1)
+dp[0] = True
+
+items = [100, 101, 102, 103, 104, 105]
+for i in range(1, X + 1):
+  for item in items:
+    idx = i - item
+    if idx >= 0 and dp[idx]:
+      dp[i] = True
+      break
+
+if dp[X]:
+  print(1)
+else:
+  print(0)
