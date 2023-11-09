49e325f3c6cd8c1e6aa51135c32fba2fb656bab4
hxrxchang <mail@hxrxchang.dev>
2023-11-10 02:02:50 +0900

    solve C

--- contests/abc136/C/main.py
+++ contests/abc136/C/main.py
@@ -1,1 +1,14 @@
+N = int(input())
+H = list(map(int, input().split()))
+
+for i in range(N - 1, 1, -1):
+  if H[i - 1] <= H[i]:
+    continue
+  elif H[i - 1] - H[i] == 1:
+    H[i - 1] -= 1
+  else:
+    print('No')
+    exit()
+
+print('Yes')
 
