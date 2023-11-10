fbca9ebe5da51d03bb22091179cbb351d736486a
hxrxchang <mail@hxrxchang.dev>
2023-11-10 11:30:05 +0900

    solve C

--- contests/abc087/C/main.py
+++ contests/abc087/C/main.py
@@ -1,1 +1,11 @@
+N = int(input())
+A1 = list(map(int, input().split()))
+A2 = list(map(int, input().split()))
 
+ans = 0
+for i in range(N):
+  a1 = sum(A1[:i+1])
+  a2 = sum(A2[i:])
+  ans = max(ans, a1+a2)
+
+print(ans)
