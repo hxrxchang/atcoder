1e32eab1d42e3e36df9954cc0fa415203ae85539
hxrxchang <mail@hxrxchang.dev>
2023-11-21 01:12:47 +0900

    solve 061

--- contests/typical90/061/main.py
+++ contests/typical90/061/main.py
@@ -1,1 +1,16 @@
+Q = int(input())
 
+from_top = []
+from_bottom = []
+
+for _ in range(Q):
+  t, x = map(int, input().split())
+  if t == 1:
+    from_top.append(x)
+  elif t == 2:
+    from_bottom.append(x)
+  else:
+    if x <= len(from_top):
+      print(from_top[-x])
+    else:
+      print(from_bottom[x - len(from_top) - 1])
