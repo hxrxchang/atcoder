60fc05cd24b1471bec50ed078a00e6396f9e978a
hxrxchang <mail@hxrxchang.dev>
2023-12-13 02:55:11 +0900

    solve (substack 1)

--- contests/typical90/065/main.py
+++ contests/typical90/065/main.py
@@ -1,1 +1,16 @@
+from itertools import combinations
 
+R, G, B, K = map(int, input().split())
+X, Y, Z = map(int, input().split())
+
+balls = ['R'] * R + ['G'] * G + ['B'] * B
+
+cnt = 0
+for c in combinations(balls, K):
+  cnt_r = c.count('R')
+  cnt_g = c.count('G')
+  cnt_b = c.count('B')
+  if cnt_r + cnt_g <= X and cnt_g + cnt_b <= Y and cnt_b + cnt_r <= Z:
+    cnt += 1
+
+print(cnt)
