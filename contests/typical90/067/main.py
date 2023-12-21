27e11131fd23860f548f6a066bbb9e3da561e1eb
hxrxchang <mail@hxrxchang.dev>
2023-12-21 21:41:41 +0900

    solve 067

--- contests/typical90/067/main.py
+++ contests/typical90/067/main.py
@@ -1,1 +1,16 @@
+from numpy import base_repr
 
+N, K = map(int, input().split())
+
+for _ in range(K):
+  dec = int(str(N), 8)
+  nine_dec = base_repr(dec, 9)
+  tmp = ""
+  for s in str(nine_dec):
+    if s == "8":
+      tmp += "5"
+    else:
+      tmp += s
+  N = int(tmp)
+
+print(N)
