194ffe504edba7b26e479ffe24b22de40df1453d
hxrxchang <mail@hxrxchang.dev>
2024-01-10 09:26:53 +0900

    solve B

--- rust/abc054/src/bin/b.rs
+++ rust/abc054/src/bin/b.rs
@@ -1,10 +1,32 @@
 use proconio::{fastout, input};
+use proconio::marker::Chars;
 
 #[fastout]
 fn main() {
     input! {
-        n: i32,
-        m: i32,
+        n: usize,
+        m: usize,
+        a: [Chars; n],
+        b: [Chars; m],
     }
-    println!("{} {}", n, m)
+    for i in 0..n - m + 1 {
+        for j in 0..n - m + 1 {
+            if check(&a, &b, i, j) {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+    println!("No");
+}
+
+fn check(a: &Vec<Vec<char>>, b: &Vec<Vec<char>>, si: usize, sj: usize) -> bool {
+    for i in 0..b.len() {
+        for j in 0..b.len() {
+            if a[i + si][j + sj] != b[i][j] {
+                return false;
+            }
+        }
+    }
+    true
 }
