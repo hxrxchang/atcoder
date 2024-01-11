a0a2d43f520a8a8eb963c21ad88750fd50e61af3
hxrxchang <mail@hxrxchang.dev>
2024-01-12 01:20:21 +0900

    solve B

--- rust/abc335/src/bin/b.rs
+++ rust/abc335/src/bin/b.rs
@@ -1,5 +1,16 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: i32,
+    }
+    for x2 in 0..=n {
+        for y2 in 0..=n {
+            for z2 in 0..=n {
+                if x2 + y2 + z2 <= n {
+                    println!("{} {} {}", x2, y2, z2);
+                }
+            }
+        }
+    }
 }
