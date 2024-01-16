de7640e63ef33d4d9eb793716e7051558a35ab3c
hxrxchang <mail@hxrxchang.dev>
2024-01-16 23:33:06 +0900

    solve 070

--- contests/typical90/070/main.py
+++ contests/typical90/070/main.py
@@ -1,1 +1,24 @@
+from statistics import median_low
 
+n = int(input())
+x = []
+y = []
+
+for _ in range(n):
+  xi, yi = map(int, input().split())
+  x.append(xi)
+  y.append(yi)
+
+x.sort()
+y.sort()
+
+ax = median_low(x)
+ay = median_low(y)
+
+ans_x = 0
+ans_y = 0
+for i in range(n):
+  ans_x += abs(x[i] - ax)
+  ans_y += abs(y[i] - ay)
+
+print(ans_x + ans_y)
