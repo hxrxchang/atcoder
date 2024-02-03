e6d735e1f7ed64c871dbb2e2b2106833ff4b3a34
hxrxchang <mail@hxrxchang.dev>
2024-02-04 03:05:07 +0900

    upsolve by rust

--- rust/abc339/src/bin/b.rs
+++ rust/abc339/src/bin/b.rs
@@ -2,69 +2,57 @@ use proconio::input;
 
 fn main() {
     input! {
-        h: usize,
-        w: usize,
+        h: i32,
+        w: i32,
         n: i32,
     }
+    let mut grid = vec![vec!['.'; w as usize]; h as usize];
+    let mut current_h: i32 = 0;
+    let mut current_w: i32 = 0;
+    // 0: up, 1: right, 2: down, 3: left
+    let mut direction = 0;
 
-    let mut grid = vec![vec!['.'; w]; h];
-    let mut direction = "right";
-    let mut tmp = (0, 1);
-    grid[0][0] = '#';
+    for _ in 0..n {
+        if grid[current_h as usize][current_w as usize] == '.' {
+            grid[current_h as usize][current_w as usize] = '#';
+            direction += 1;
+        } else {
+            grid[current_h as usize][current_w as usize] = '.';
+            direction -= 1;
+        }
+        direction %= 4;
+        if direction < 0 {
+            direction += 4;
+        }
 
-    for _ in 0..n-1 {
-        let (h, w) = tmp;
         match direction {
-            "right" => {
-                if grid[h][w] == '.' {
-                    grid[h][w] = '#';
-                    direction = "down";
-                    tmp = (h + 1, w);
-                } else {
-                    grid[h][w] = '.';
-                    direction = "up";
-                    tmp = (h - 1, w);
-                }
-            },
-            "down" => {
-                if grid[h][w] == '.' {
-                    grid[h][w] = '#';
-                    direction = "left";
-                    tmp = (h, w - 1);
-                } else {
-                    grid[h][w] = '.';
-                    direction = "right";
-                    tmp = (h, w + 1);
-                }
-            },
-            "left" => {
-                if grid[h][w] == '.' {
-                    grid[h][w] = '#';
-                    direction = "up";
-                    tmp = (h - 1, w);
-                } else {
-                    grid[h][w] = '.';
-                    direction = "down";
-                    tmp = (h + 1, w);
-                }
-            },
-            "up" => {
-                if grid[h][w] == '.' {
-                    grid[h][w] = '#';
-                    direction = "right";
-                    tmp = (h, w + 1);
-                } else {
-                    grid[h][w] = '.';
-                    direction = "left";
-                    tmp = (h, w - 1);
-                }
-            },
-            _ => {}
+            0 => {
+                current_h -= 1;
+            }
+            1 => {
+                current_w += 1;
+            }
+            2 => {
+                current_h += 1;
+            }
+            3 => {
+                current_w -= 1;
+            }
+            _ => {
+                unreachable!();
+            }
+        }
+        current_h %= h;
+        if current_h < 0 {
+            current_h += h;
+        }
+        current_w %= w;
+        if current_w < 0 {
+            current_w += w;
         }
     }
 
-
-    for row in grid.iter() {
+    for row in grid {
         println!("{}", row.iter().collect::<String>());
     }
 }
