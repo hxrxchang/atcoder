b472045092d73ea4c777490a1fc09334f7fbc6b0
hxrxchang <mail@hxrxchang.dev>
2024-02-05 22:26:07 +0900

    solve D

---
+++ rust/abc323/src/bin/d.rs
@@ -0,0 +1,33 @@
+use proconio::input;
+use std::collections::{BinaryHeap, HashMap};
+
+fn main() {
+    input! {
+        n: usize,
+        slimes: [(i64, i64); n]
+    }
+
+    let mut heap: BinaryHeap<i64> = BinaryHeap::new();
+    let mut counts = HashMap::new();
+
+    for s in slimes {
+        let (slime_name, cnt) = s;
+        heap.push(slime_name);
+        *counts.entry(slime_name).or_insert(0) += cnt;
+    }
+
+    while let Some(s) = heap.pop() {
+        let slime_name = s;
+        let slime_cnt = counts.get(&slime_name).unwrap();
+        if slime_cnt >= &2 {
+            let next_slime_cnt = slime_cnt / 2;
+            let next_slime_name: i64 = slime_name * 2;
+            heap.push(next_slime_name);
+            *counts.entry(slime_name).or_insert(0) = slime_cnt % 2;
+            *counts.entry(next_slime_name).or_insert(0) += next_slime_cnt;
+        }
+    }
+
+    let result: i64 = counts.values().sum();
+    println!("{}", result);
+}
