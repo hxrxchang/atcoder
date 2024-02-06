067b92607cd63b57dd0c18a228d40881f1744d2d
hxrxchang <mail@hxrxchang.dev>
2024-02-06 19:07:12 +0900

    fix

--- rust/abc323/src/bin/d.rs
+++ rust/abc323/src/bin/d.rs
@@ -1,5 +1,5 @@
 use proconio::input;
-use std::collections::{BinaryHeap, HashMap};
+use std::{cmp::Reverse, collections::{BinaryHeap, HashMap}};
 
 fn main() {
     input! {
@@ -7,22 +7,22 @@ fn main() {
         slimes: [(i64, i64); n]
     }
 
-    let mut heap: BinaryHeap<i64> = BinaryHeap::new();
+    let mut heap = BinaryHeap::new();
     let mut counts = HashMap::new();
 
     for s in slimes {
         let (slime_name, cnt) = s;
-        heap.push(slime_name);
+        heap.push(Reverse(slime_name));
         *counts.entry(slime_name).or_insert(0) += cnt;
     }
 
     while let Some(s) = heap.pop() {
-        let slime_name = s;
+        let slime_name = s.0;
         let slime_cnt = counts.get(&slime_name).unwrap();
         if slime_cnt >= &2 {
             let next_slime_cnt = slime_cnt / 2;
             let next_slime_name: i64 = slime_name * 2;
-            heap.push(next_slime_name);
+            heap.push(Reverse(next_slime_name));
             *counts.entry(slime_name).or_insert(0) = slime_cnt % 2;
             *counts.entry(next_slime_name).or_insert(0) += next_slime_cnt;
         }
