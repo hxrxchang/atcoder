ada7ac34fd4def3fd2f479bcd7947b1361ae75d1
hxrxchang <mail@hxrxchang.dev>
2024-02-14 13:46:25 +0900

    solve 075

--- contests/typical90/075/main.py
+++ contests/typical90/075/main.py
@@ -1,1 +1,24 @@
+import math
 
+N = int(input())
+
+def prime_factorize(n):
+  a = []
+  while n % 2 == 0:
+    a.append(2)
+    n //= 2
+  f = 3
+  while f * f <= n:
+    if n % f == 0:
+      a.append(f)
+      n //= f
+    else:
+      f += 2
+  if n != 1:
+    a.append(n)
+  return a
+
+primes = prime_factorize(N)
+ans = math.ceil(math.log2(len(primes)))
+
+print(ans)
