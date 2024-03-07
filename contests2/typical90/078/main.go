8c6361a0100b9b742398706a17b54fb2e7cbead3
hxrxchang <mail@hxrxchang.dev>
2024-03-07 10:40:47 +0900

    resolve by go

---
+++ contests2/typical90/078/main.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const BUFSIZE = 10000000
+var rdr *bufio.Reader
+
+func main() {
+	rdr = bufio.NewReaderSize(os.Stdin, BUFSIZE)
+	solve()
+}
+
+func solve() {
+	l := mapToIntSlice(input())
+	n, m := l[0], l[1]
+
+	graph := make([][]int, n)
+	for i := 0; i < m; i++ {
+		l = mapToIntSlice(input())
+		a, b := l[0] - 1, l[1] - 1
+		if a > b {
+			graph[a] = append(graph[a], b)
+		} else {
+			graph[b] = append(graph[b], a)
+		}
+	}
+	ans := 0
+	for _, node := range graph {
+		if len(node) == 1 {
+			ans++
+		}
+	}
+
+	fmt.Println(ans)
+}
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// 一行をstringで読み込み
+func input() string {
+	buf := make([]byte, 0, 16)
+	for {
+		l, p, e := rdr.ReadLine()
+		if e != nil {
+			fmt.Println(e.Error())
+			panic(e)
+		}
+		buf = append(buf, l...)
+		if !p {
+			break
+		}
+	}
+	return string(buf)
+}
+
+// string <-> []string
+// 第２引数で渡された文字列でsplitする
+func strToSlice(input, sep string) []string {
+	return strings.Split(input, sep)
+}
+
+
+// // string <-> []int
+func mapToIntSlice(input string) []int {
+	slice := make([]int, 0)
+	lines := strToSlice(input, " ")
+	for _, v := range lines {
+		// s2iはstringをintに変換する関数(後述)
+		slice = append(slice, s2i(v))
+	}
+	return slice
+}
+
+// string <-> int
+func s2i(s string) int {
+	v, ok := strconv.Atoi(s)
+	if ok != nil {
+		panic("Faild : " + s + " can't convert to int")
+	}
+	return v
+}
+
+func i2s(i int) string {
+	return strconv.Itoa(i)
+}
+
+// bool <-> int
+func b2i(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
+}
+
+func i2b(i int) bool {
+  return i != 0
+}
+
+func abs(v int) int {
+	if v < 0 {
+		return -v
+	}
+	return v
+}
+
+func min(values ...int) int {
+	ret := values[0]
+	for _, v := range values {
+		if ret > v {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func max(values ...int) int {
+	ret := values[0]
+	for _, v := range values {
+		if ret < v {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func mod(x, y int) int {
+	m := x % y
+	if m < 0 {
+		return m + y
+	}
+	return m
+}
+
+func pow(x, y int) int {
+	return int(math.Pow(float64(x), float64(y)))
+}
+
+func gcd(v1, v2 int) int {
+	if v1 > v2 {
+		v1, v2 = v2, v1
+	}
+	for v1 != 0 {
+		v1, v2 = v2%v1, v1
+	}
+	return v2
+}
+
+func lcm(v1, v2 int) int {
+	return v1 * v2 / gcd(v1, v2)
+}
+
+// heap (priority queue)
+type intHeap []int
+func (h intHeap) Len() int {
+	return len(h)
+}
+func (h intHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+func (h intHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+func (h *intHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+func (h *intHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
