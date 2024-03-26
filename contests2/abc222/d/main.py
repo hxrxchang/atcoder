1e4a19462d968e011a7489be5b7f29da2f92f9e5
hxrxchang <mail@hxrxchang.dev>
2024-03-26 20:24:53 +0900

    solve D

--- contests2/abc222/d/main.py
+++ contests2/abc222/d/main.py
@@ -2,17 +2,19 @@ N = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 MOD = 998244353
+M = 3001
 
-# dp[i]: A, Bのi番目までの見たときにC[i]になりえる最小値、最大値、個数
-dp = [{'max': 0, 'min': 0, 'count': 0} for _ in range(N)]
-dp[0]['max'] = B[0]
-dp[0]['min'] = A[0]
-dp[0]['count'] = (B[0] - A[0] + 1) % MOD
+# dp[i][j]: Cのi番目までの数列で、Cのi番目がjであるパターン数
+dp = [[0] * M for _ in range(N + 1)]
+dp[0][0] = 1
 
-for i in range(1, N):
-    dp[i]['max'] = B[i]
-    dp[i]['min'] = A[i]
-    dp[i]['count'] = dp[i - 1]['count'] * (dp[i]['max'] - dp[i]['min'] + 1) % MOD
+for i in range(N):
+    a = A[i]
+    b = B[i]
+    s = sum(dp[i][:a]) % MOD
+    for c in range(a, b + 1):
+        s += dp[i][c]
+        s %= MOD
+        dp[i + 1][c] = s
 
-print(dp[N - 1]['count'])
-
+print(sum(dp[N]) % MOD)
