4dea90271b25b1ebcab4692b05be97f444620f8d
hxrxchang <mail@hxrxchang.dev>
2024-05-05 00:55:33 +0900

    solve d

--- contests/abc344/d/main.py
+++ contests/abc344/d/main.py
@@ -1,9 +1,23 @@
 T = input()
 N = int(input())
 
-bags = [list(map(int, input().split())) for _ in range(N)]
+bags = []
+for _ in range(N):
+    inp = input().split()
+    inp[0] = int(inp[0])
+    bags.append(inp)
 
-dp = [(0, 0)] * (len(T) + 1)
+# dp[i]: Tのi文字目までをいくらで作れるか(最小金額)
+dp = [float('inf')] * (len(T) + 1)
+dp[0] = 0
 
 for i in range(N):
+    tmp = dp[:]
+    for item in bags[i][1:]:
+        for j, d in enumerate(dp):
+            if d == float('inf'):
+                continue
+            if item == T[j:j + len(item)]:
+                dp[j + len(item)] = min(tmp[j + len(item)], tmp[j] + 1)
 
+print(dp[-1] if dp[-1] != float('inf') else -1)
