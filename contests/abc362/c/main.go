a46c7ee8d7f25c370c7539f6f8805cf45ea37787
hxrxchang <mail@hxrxchang.dev>
2024-09-28 10:31:41 +0900

    solve c

--- contests/abc362/c/main.go
+++ contests/abc362/c/main.go
@@ -21,6 +21,7 @@ import (
 const BUFSIZE = 10000000
 const MOD = 1000000007
 const BIGGEST = math.MaxInt64
+const MINIMUM = math.MinInt64
 var rdr *bufio.Reader
 
 func main() {
@@ -29,6 +30,49 @@ func main() {
 }
 
 func solve() {
+	n := getInt()
+	ls := make([]int, n)
+	rs := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		in := getInts()
+		l, r := in[0], in[1]
+		ls[i] = l
+		rs[i] = r
+	}
+
+	sumLs := sum(ls)
+	sumRs := sum(rs)
+
+	if sumLs > 0 || sumRs < 0 {
+		fmt.Println("No")
+		return
+	}
+
+	s := sumLs
+	ans := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		l, r := ls[i], rs[i]
+		d := min(r-l, -s)
+		s += d
+		ans[i] = l + d
+	}
+
+	if s != 0 {
+		fmt.Println("No")
+	} else {
+		fmt.Println("Yes")
+		printSlice(ans)
+	}
+}
+
+func sum(slice []int) int {
+	sum := 0
+	for _, v := range slice {
+		sum += v
+	}
+	return sum
 }
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -90,15 +134,32 @@ func mapToIntSlice(input string) []int {
 
 // string <-> int
 func s2i(s string) int {
-	v, ok := strconv.Atoi(s)
-	if ok != nil {
+	v, err := strconv.Atoi(s)
+	if err != nil {
 		panic("Faild : " + s + " can't convert to int")
+	}
+	return v
+}
+
+func s2float64(s string) float64 {
+	v, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		panic("Faild : " + s + " can't convert to float64")
 	}
 	return v
 }
 
 func i2s(i int) string {
 	return strconv.Itoa(i)
+}
+
+func isPalindrome(s string) bool {
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] != s[len(s)-1-i] {
+			return false
+		}
+	}
+	return true
 }
 
 // bool <-> int
@@ -111,6 +172,10 @@ func b2i(b bool) int {
 
 func i2b(i int) bool {
   return i != 0
+}
+
+func i2bit(i int) []string {
+	return strToSlice(strconv.FormatInt(int64(i), 2), "")
 }
 
 func abs(v int) int {
@@ -172,6 +237,17 @@ func modPow(base, exp, mod int) int {
 	return result
 }
 
+// logXのYを求める
+func logXY(x, y int) int {
+	return int(math.Log(float64(y)) / math.Log(float64(x)))
+}
+
+// intのまま計算できるように
+func sqrt(x int) int {
+	return int(math.Sqrt(float64(x)))
+}
+
+// 最大公約数
 func gcd(v1, v2 int) int {
 	if v1 > v2 {
 		v1, v2 = v2, v1
@@ -182,15 +258,102 @@ func gcd(v1, v2 int) int {
 	return v2
 }
 
+// 最小公倍数
 func lcm(v1, v2 int) int {
 	return v1 * v2 / gcd(v1, v2)
 }
 
+// 切り上げ除算
 func ceilDiv(a, b int) int {
 	if a + b - 1 < 0 && (a + b - 1) % b != 0 {
 		return (a + b - 1) / b - 1
 	}
 	return (a + b - 1) / b
+}
+
+// nCr
+func getComb(n, k int) int {
+	numerator := 1
+	denominator := 1
+	for i := 0; i < k; i++ {
+		numerator *= n - i
+		denominator *= i + 1
+	}
+	return numerator / denominator
+}
+
+// nを素因数分解
+func primeFactorize(n int) []int {
+	var factors []int
+
+	// 2で割り切れる間、2を追加
+	for n%2 == 0 {
+		factors = append(factors, 2)
+		n /= 2
+	}
+
+	// 3以降の奇数で割り切れるか確認
+	for f := 3; f*f <= n; f += 2 {
+		for n%f == 0 {
+			factors = append(factors, f)
+			n /= f
+		}
+	}
+
+	// nが1でない場合は、n自身を追加
+	if n > 1 {
+		factors = append(factors, n)
+	}
+
+	return factors
+}
+
+// n以下の素数を列挙
+func primeNumbers(n int) []int {
+	isPrime := getIsPrime(n)
+
+	primes := make([]int, 0)
+	for i, b := range isPrime {
+		if b {
+			primes = append(primes, i)
+		}
+	}
+
+	return primes
+}
+
+// n以下の数字がそれぞれ素数かどうかを列挙
+func getIsPrime(n int) []bool {
+	isPrime := make([]bool, n+1)
+	for i := 0; i <= n; i++ {
+		isPrime[i] = true
+	}
+	isPrime[0] = false
+	isPrime[1] = false
+
+	for i := 2; i <= n; i++ {
+		if isPrime[i] {
+			for j := i * 2; j <= n; j += i {
+				isPrime[j] = false
+			}
+		}
+	}
+	return isPrime
+}
+
+// nが素数かどうかを判定
+func isPrime(n int) bool {
+	if n == 2 {
+		return true
+	} else if n < 2 || n%2 == 0 {
+		return false
+	}
+	for i := 3; i*i <= n; i += 2 {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
 
 // set
@@ -213,6 +376,18 @@ func (s *Set[V]) Has(v V) bool {
 func (s *Set[V]) Values() []V {
 	return maps.Keys(s.values)
 }
+func (s *Set[V]) Size() int {
+	return len(s.values)
+}
+func(s *Set[V]) Pop() V {
+	if len(s.values) == 0 {
+		panic("set is empty")
+	}
+	v := s.Values()[0]
+	s.Remove(v)
+	return v
+}
+
 
 // sorted set
 type SortedSet[T comparator.Ordered] struct {
@@ -239,6 +414,21 @@ func (s *SortedSet[T]) lowerBound(v T) *set.SetIterator[T] {
 }
 func (s *SortedSet[T]) upperBound(v T) *set.SetIterator[T] {
 	return s.values.UpperBound(v)
+}
+// 指定した値未満の最大の値を取得
+func (s *SortedSet[T]) lessThan(v T) (*T, error) {
+	if s.lowerBound(v).Prev().IsValid() {
+		res := s.lowerBound(v).Prev().Value()
+		return &res, nil
+	}
+	if s.values.Last().IsValid() {
+		res := s.values.Last().Value()
+		if res < v {
+			return &res, nil
+		}
+		return nil, fmt.Errorf("not found")
+	}
+	return nil, fmt.Errorf("not found")
 }
 
 // multiset
@@ -288,6 +478,67 @@ func (h *MyHeap[T]) len() int {
 	return h.heap.Len()
 }
 
+// tuple heap
+type TupleHeap[T constraints.Ordered] [][]T
+func (th TupleHeap[T]) Len() int {
+	return len(th)
+}
+func (th TupleHeap[T]) Less(i, j int) bool {
+	return th.compareSlices(i, j, 0)
+}
+func (th TupleHeap[T]) Swap(i, j int) {
+	th[i], th[j] = th[j], th[i]
+}
+func (th *TupleHeap[T]) Push(x any) {
+	*th = append(*th, x.([]T))
+}
+func (h *TupleHeap[T]) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+func (th TupleHeap[T]) compareSlices(i, j, idx int) bool {
+	a := th[i]
+	b := th[j]
+	// 片方のスライスが他方よりも短ければ、その時点で比較を終了
+	if len(a) <= idx && len(b) > idx {
+		return true
+	} else if len(a) > idx && len(b) <= idx {
+		return false
+	} else if len(a) <= idx && len(b) <= idx {
+		return false // どちらも同じ長さで全ての要素が等しい場合
+	}
+
+	// 現在のインデックスの値を比較
+	if a[idx] != b[idx] {
+		return a[idx] < b[idx]
+	}
+
+	// 同じ値の場合、次のインデックスを再帰的に比較
+	return th.compareSlices(i, j, idx+1)
+}
+
+type MyTupleHeap[T constraints.Ordered] struct {
+	heap TupleHeap[T]
+}
+func newMyTupleHeap[T constraints.Ordered]() *MyTupleHeap[T] {
+	myTupleHeap := &MyTupleHeap[T]{}
+	heap.Init(&myTupleHeap.heap)
+	return myTupleHeap
+}
+func (h *MyTupleHeap[T]) push(x []T) {
+	heap.Push(&h.heap, x)
+}
+func (h *MyTupleHeap[T]) pop() []T {
+	return heap.Pop(&h.heap).([]T)
+}
+func (h *MyTupleHeap[T]) len() int {
+	return h.heap.Len()
+}
+
+
 func sortSlice[T constraints.Ordered](slice []T) []T {
     copiedSlice := make([]T, len(slice))
     copy(copiedSlice, slice)
@@ -335,6 +586,21 @@ func rangeSlice2(start, end int) []int {
 		slice[i - start] = i
 	}
 	return slice
+}
+
+// intのsliceを0indexに変換
+func zeroIndexedSlice(origin []int) []int {
+	slice2 := make([]int, len(origin))
+	for i, v := range origin {
+		slice2[i] = v - 1
+	}
+	return slice2
+}
+
+func copySlice[T any](original []T) []T {
+	newSlice := make([]T, len(original))
+	copy(newSlice, original)
+	return newSlice
 }
 
 // 2次元スライスのコピー
@@ -491,44 +757,6 @@ func getCombinationsCh(list []int, k int) (c chan []int) {
 	return
 }
 
-// nCr
-func getComb(n, k int) int {
-	numerator := 1
-	denominator := 1
-	for i := 0; i < k; i++ {
-		numerator *= n - i
-		denominator *= i + 1
-	}
-	return numerator / denominator
-}
-
-// n以下の素数を列挙
-func primeNumbers(n int) []int {
-	isPrime := make([]bool, n+1)
-	for i := 0; i <= n; i++ {
-		isPrime[i] = true
-	}
-	isPrime[0] = false
-	isPrime[1] = false
-
-	for i := 2; i <= n; i++ {
-		if isPrime[i] {
-			for j := i * 2; j <= n; j += i {
-				isPrime[j] = false
-			}
-		}
-	}
-
-	primes := make([]int, 0)
-	for i, b := range isPrime {
-		if b {
-			primes = append(primes, i)
-		}
-	}
-
-	return primes
-}
-
 // bit全探索
 func generateSubsets[T any](elements []T) [][]T {
 	n := len(elements)
@@ -559,6 +787,76 @@ func bisectLeft[T constraints.Ordered](slice []T, value T) int {
 // sliceの中でvalueより大きい値が最初に現れるindexを返す
 func bisectRight[T constraints.Ordered](slice []T, value T) int {
 	return bisect(slice, func(i int) bool { return slice[i] > value })
+}
+// sliceの中で指定した値以上の要素を返す
+func lowerBound[T constraints.Ordered](slice []T, value T) *T {
+	idx := bisectLeft(slice, value)
+	if slice[idx] == value {
+		return &value
+	}
+	if idx == 0 {
+		return nil
+	}
+	return &slice[idx-1]
+}
+// sliceの中で指定した値より大きい要素を返す
+func upperBound[T constraints.Ordered](slice []T, value T) *T {
+	idx := bisectRight(slice, value)
+	if idx == len(slice) {
+		return nil
+	}
+	return &slice[idx]
+}
+// sliceの中で指定した値未満の中で最大の要素を返す
+func lessThan[T constraints.Ordered](slice []T, value T) *T {
+	idx := bisectLeft(slice, value)
+	if idx == 0 {
+		return nil
+	}
+	return &slice[idx-1]
+}
+
+// 座標圧縮
+type Zaatsu struct {
+	values []int
+	mapping map[int]int
+}
+func newZaatsu(params []int) *Zaatsu {
+	s := newSet[int]()
+	for _, v := range params {
+		s.Add(v)
+	}
+	sorted := sortSlice(s.Values())
+
+	mapping := make(map[int]int)
+	for i, v := range sorted {
+		mapping[v] = i
+	}
+
+	return &Zaatsu{
+		values: sorted,
+		mapping: mapping,
+	}
+}
+// 圧縮後の値を取得
+func (z *Zaatsu) GetCompressedValue(v int) int {
+	return z.mapping[v]
+}
+// 圧縮後の値から元の値を取得
+func (z *Zaatsu) GetOriginalValue(compressedIndex int) int {
+	if compressedIndex < 0 || compressedIndex >= len(z.values) {
+		panic("Index out of range")
+	}
+	return z.values[compressedIndex]
+}
+func (z *Zaatsu) Count() int {
+	return len(z.values)
+}
+func (z *Zaatsu) BisectLeft(v int) int {
+	return bisectLeft(z.values, v)
+}
+func (z *Zaatsu) BisectRight(v int) int {
+	return bisectRight(z.values, v)
 }
 
 // Segment Tree
@@ -609,6 +907,201 @@ func (segtree *SegmentTree[T]) Query(begin, end int) T {
 	return segtree.query(begin, end, 0, 0, segtree.n)
 }
 
+// 単純なgraphのDFS
+func graphBfs(graph [][]int, start int) []int {
+	size := len(graph)
+	que := newQueue[int]()
+	distances := make([]int, size)
+	for i := 0; i < size; i++ {
+		distances[i] = -1
+	}
+	distances[start] = 0
+	que.PushBack(start)
+	for que.Size() > 0 {
+		v := que.PopFront()
+		for _, next := range graph[v] {
+			if distances[next] != -1 {
+				continue
+			}
+			distances[next] = distances[v] + 1
+			que.PushBack(next)
+		}
+	}
+	return distances
+}
+
+// gridのDFS
+type GridBfsNode struct {
+	y, x int
+}
+func gridBfs(height, width int, nextNodes map[GridBfsNode][]GridBfsNode, start GridBfsNode) [][]int {
+	type Item struct {
+		item GridBfsNode
+		dist int
+	}
+	distances := make([][]int, height)
+	for i := 0; i < height; i++ {
+		distances[i] = make([]int, width)
+		for j := 0; j < width; j++ {
+			distances[i][j] = -1
+		}
+	}
+
+	distances[start.y][start.x] = 0
+
+	que := newQueue[Item]()
+	que.PushBack(Item{GridBfsNode{start.y, start.x}, 0})
+
+	for que.Size() > 0 {
+		current := que.PopFront()
+		y, x, dist := current.item.y, current.item.x, current.dist
+		for _, next := range nextNodes[GridBfsNode{y, x}] {
+			if next.y < 0 || next.y >= height || next.x < 0 || next.x >= width {
+				continue
+			}
+			if distances[next.y][next.x] != -1 {
+				continue
+			}
+			distances[next.y][next.x] = dist + 1
+			que.PushBack(Item{next, dist + 1})
+		}
+	}
+
+	return distances
+}
+
+
+// ワーシャルフロイド法
+func warshallFloyd(graph [][]int) [][]int {
+	n := len(graph)
+	dist := make([][]int, n)
+	for i := range dist {
+		dist[i] = make([]int, n)
+		for j := range dist[i] {
+			if i == j {
+				dist[i][j] = 0
+			} else if graph[i][j] == 0 {
+				dist[i][j] = BIGGEST
+			} else {
+				dist[i][j] = graph[i][j]
+			}
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			for k := 0; k < n; k++ {
+				if dist[j][k] > dist[j][i]+dist[i][k] {
+					dist[j][k] = dist[j][i] + dist[i][k]
+				}
+			}
+		}
+	}
+
+	return dist
+}
+
+// ダイクストラ法
+func dijkstra(graph [][]dijkstraItem, start int) []int {
+	n := len(graph)
+	dist := make([]int, n)
+	for i := range dist {
+		dist[i] = BIGGEST
+	}
+	dist[start] = 0
+
+	pq := &dijkstraPriorityQueue{}
+	heap.Init(pq)
+	heap.Push(pq, &dijkstraItem{node: start, dist: 0})
+
+	for pq.Len() > 0 {
+		u := heap.Pop(pq).(*dijkstraItem)
+		if u.dist > dist[u.node] {
+			continue
+		}
+
+		for _, edge := range graph[u.node] {
+			v := edge.node
+			alt := u.dist + edge.dist
+			if alt < dist[v] {
+				dist[v] = alt
+				heap.Push(pq, &dijkstraItem{node: v, dist: alt})
+			}
+		}
+	}
+
+	return dist
+}
+type dijkstraItem struct {
+	node int
+	dist   int
+}
+type dijkstraPriorityQueue []*dijkstraItem
+func (pq dijkstraPriorityQueue) Len() int { return len(pq) }
+func (pq dijkstraPriorityQueue) Less(i, j int) bool {
+	return pq[i].dist < pq[j].dist
+}
+func (pq dijkstraPriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+func (pq *dijkstraPriorityQueue) Push(x interface{}) {
+	*pq = append(*pq, x.(*dijkstraItem))
+}
+func (pq *dijkstraPriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	*pq = old[0 : n-1]
+	return item
+}
+
+// ローリングハッシュ
+func NewRollingHash(S string) *RollingHash {
+	const base1, base2 = 1007, 2009
+	const mod1, mod2 = 1000000007, 1000000009
+
+	n := len(S)
+	hash1 := make([]int64, n+1)
+	hash2 := make([]int64, n+1)
+	power1 := make([]int64, n+1)
+	power2 := make([]int64, n+1)
+	power1[0], power2[0] = 1, 1
+
+	for i := 0; i < n; i++ {
+		hash1[i+1] = (hash1[i]*base1 + int64(S[i])) % mod1
+		hash2[i+1] = (hash2[i]*base2 + int64(S[i])) % mod2
+		power1[i+1] = (power1[i] * base1) % mod1
+		power2[i+1] = (power2[i] * base2) % mod2
+	}
+
+	return &RollingHash{
+		base1:  base1,
+		base2:  base2,
+		mod1:   mod1,
+		mod2:   mod2,
+		hash1:  hash1,
+		hash2:  hash2,
+		power1: power1,
+		power2: power2,
+	}
+}
+type RollingHash struct {
+	base1, base2 int64
+	mod1, mod2   int64
+	hash1, hash2 []int64
+	power1, power2 []int64
+}
+type RollingHashPair struct {
+	Hash1 int64
+	Hash2 int64
+}
+// S[left:right] のハッシュ値を取得
+func (rh *RollingHash) Get(l, r int) RollingHashPair {
+	res1 := (rh.hash1[r] - rh.hash1[l]*rh.power1[r-l]%rh.mod1 + rh.mod1) % rh.mod1
+	res2 := (rh.hash2[r] - rh.hash2[l]*rh.power2[r-l]%rh.mod2 + rh.mod2) % rh.mod2
+	return RollingHashPair{res1, res2}
+}
+
 
 // sliceを一行で出力
 func printSlice[T any](data []T) {
@@ -631,4 +1124,18 @@ func isSubstring(s, t string) bool {
 		}
 	}
 	return ok
+}
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//　幾何ゾーン
+
+// 2点間の距離の2乗
+// 平方根を取ると距離になるが、誤差が出るので距離の比較は距離の2乗で行う
+func distanceSquared(x1, y1, x2, y2 int) int {
+	return pow(x1-x2, 2) + pow(y1-y2, 2)
+}
+
+// 3点が同一直線上にあるか判定
+func isOnSameLine(x1, y1, x2, y2, x3, y3 int) bool {
+	return (x1-x2)*(y2-y3) == (y1-y2)*(x2-x3)
 }
