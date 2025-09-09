ee35c7b1a658d00456f0dba66eeafe9d8a6acae6
hxrxchang <mail@hxrxchang.dev>
2025-09-09 16:01:38 +0900

    solve a

---
+++ rust/abc420/src/bin/a.rs
@@ -0,0 +1,14 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        x: i32,
+        y: i32,
+    }
+    let ans = (x + y) % 12;
+    if ans == 0 {
+        println!("12");
+    } else {
+        println!("{}", ans);
+    }
+}
