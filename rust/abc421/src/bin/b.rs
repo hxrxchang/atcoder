3e4afa90658095258476ee99b0a4eab78d994bba
hxrxchang <mail@hxrxchang.dev>
2025-09-10 12:06:58 +0900

    solve b

--- rust/abc421/src/bin/b.rs
+++ rust/abc421/src/bin/b.rs
@@ -1,6 +1,25 @@
 use proconio::input;
 
 fn main() {
-    input! {}
-    println!("Hello world!")
+    input! {
+        x: i64,
+        y: i64,
+    }
+
+    let mut a = Vec::new();
+    a.push(x);
+    a.push(y);
+
+    for i in 2..10 {
+        a.push(f(&a, i));
+    }
+
+    println!("{}", a[9])
+}
+
+fn f(v: &Vec<i64>, i: usize) -> i64 {
+    let x = v[i-1] + v[i-2];
+    let s = x.to_string();
+    let rev: String = s.chars().rev().collect();
+    rev.parse::<i64>().unwrap()
 }
