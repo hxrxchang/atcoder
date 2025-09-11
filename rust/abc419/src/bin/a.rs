395f5b626c755388484165a77c57d5d680e08a3f
hxrxchang <mail@hxrxchang.dev>
2025-09-11 10:29:32 +0900

    solve a

---
+++ rust/abc419/src/bin/a.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
+fn main() {
+    input! {
+        s: String,
+    }
+    let mp = HashMap::from([
+        ("red", "SSS"),
+        ("blue", "FFF"),
+        ("green", "MMM")
+    ]);
+
+    let ans = *mp.get(s.as_str()).unwrap_or(&"Unknown");
+    println!("{}", ans);
+}
