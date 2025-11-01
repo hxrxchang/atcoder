f4e23e21212ae365c9de9957e0666e9c6e77306d
hxrxchang <mail@hxrxchang.dev>
2025-11-02 01:50:18 +0900

    fix

--- contests/abc347/b/main.go
+++ contests/abc347/b/main.go
@@ -31,15 +31,15 @@ func main() {
 
 func solve() {
 	s := strToSlice(getStr(), "")
-	ans := newSet[string]()
+	st := newSet[string]()
 	for i := 0; i < len(s); i++ {
-		for j := i + 1; j < len(s)+1; j++ {
-			ans.Add(sliceToStr(s[i:j], ""))
+		for j := i; j < len(s); j++ {
+			st.Add(sliceToStr(s[i:j+1], ""))
 		}
 	}
-	fmt.Println(ans.Size())
-}
 
+	fmt.Println(st.Size())
+}
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 func getInt() int {
